7a407317d84d60df57f155f8adcdeb5dbdfff438
Darius Jakaitis <105962203+gripas@users.noreply.github.com>
2023-05-25 23:10:24 +0300

    Update login.php
    translate add

--- login.php
+++ login.php
@@ -3,7 +3,11 @@ session_start();
 require_once 'security/config.php';
 require_once 'core/functions/functions.php';
 secureSession();
-
+// Duomenų gavimas iš duomenų bazės
+$db = getDBConnection($config);
+// Gaunamas kalbos nustatymas iš duomenų bazės  
+$language_code = getLanguageSetting($db);
+$translations = getTranslations($db, $language_code);
 if (isset($_SESSION['user_id'])) {
     header('Location: index.php');
     exit();
@@ -27,7 +31,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         header('Location: /');
         exit();
     } else {
-        $error_message = 'Neteisingas vartotojo vardas arba slaptažodis.';
+        $error_message = t('Invalid username or password.');
     }
 }
 ?>
@@ -43,12 +47,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <div class="container">
     <div class="row">
         <div class="col-sm-4">
-        <h2>Prisijungimo sistema</h2>
-        <p>Turinio valdymo sistema skirta asmeninėms svetainėms</p>
+        <h2><?php echo t("Login system");?></h2>
+        <p><?php echo t("The content management system is intended for personal websites");?>s</p>
     <?php require_once 'assets/logo.php'?> 
 </div>
         <div class="col-sm-4">
-<h1 class="h3 mb-3 fw-normal">Prisijungimas</h1>
+<h1 class="h3 mb-3 fw-normal"><?php echo t("Connect to page"); ?></h1>
 <?php if (isset($error_message)): ?>
     <div class="alert alert-danger" role="alert">
         <?php echo htmlspecialchars($error_message, ENT_QUOTES, 'UTF-8'); ?>
@@ -58,21 +62,21 @@ $csrf_token = generateCSRFToken();
 ?>
 <form method="POST" action="">
     <div class="form-floating">
-        <input type="text" class="form-control" id="username" name="username" placeholder="Vartotojo vardas" required>
-        <label for="username">Vartotojo vardas</label>
+        <input type="text" class="form-control" id="username" name="username" placeholder="<?php echo t("User Name"); ?>" required>
+        <label for="username"><?php echo t("User Name"); ?></label>
     </div>
     <br>
     <div class="form-floating">
-        <input type="password" class="form-control" id="password" name="password" placeholder="Slaptažodis" required>
-        <label for="password">Slaptažodis</label>
+        <input type="password" class="form-control" id="password" name="password" placeholder="<?php echo t("{p}assword"); ?>" required>
+        <label for="password"><?php echo t("Password"); ?></label>
     </div>
     <input type="hidden" name="csrf_token" value="<?php echo htmlspecialchars($csrf_token, ENT_QUOTES, 'UTF-8'); ?>">
 
     <br>
     <!-- Įterpkite reCAPTCHA kodą čia -->
-    <button class="w-100 btn btn-lg btn-primary" type="submit">Prisijungti</button>
+    <button class="w-100 btn btn-lg btn-primary" type="submit"><?php echo t("Login"); ?></button>
 </form>
-<p>Į pagrindinį puslapį &nbsp;<a href="/" class="btn btn-link">Home page</a>&nbsp;<p>
+<p><?php echo t("Back to"); ?> &nbsp;<a href="/" class="btn btn-link"><?php echo t("Home page"); ?></a>&nbsp;<p>
 </div>
 </div>
 </div>
