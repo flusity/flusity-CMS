92b9e19a3d2b055d67ea5e116bfdaf3c9ef32e92
Darius Jakaitis <105962203+gripas@users.noreply.github.com>
2023-05-26 10:12:24 +0300

    Update register.php
    Translate add

--- register.php
+++ register.php
@@ -24,17 +24,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         if ($password === $confirm_password) {
             if (isUsernameTaken($username, $db)) {
-                $error_message = 'Vartotojo vardas jau užimtas. Pasirinkite kitą.';
+                $error_message = t("That Name is already taken. Choose another.");
             } else {
                 if (registerUser($username, $password, $surname, $phone, $email, $db)) {
                     header('Location: login.php');
                     exit();
                 } else {
-                    $error_message = 'Vartotojo registracija nepavyko. Bandykite dar kartą.';
+                    $error_message = t("User registration failed. Try again.");
                 }
             }
         } else {
-            $error_message = 'Slaptažodžiai nesutampa. Bandykite dar kartą.';
+            $error_message = t("Passwords do not match. Try again.");
         }
     }
 }
@@ -52,12 +52,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <div class="container">
     <div class="row">
         <div class="col-sm-4">
-        <h2>Registracijos sistema</h2>
-        <p>Turinio valdymo sistema skirta asmeninėms svetainėms</p>
+        <h2><?php echo t("Registration system");?></h2>
+        <p><?php echo t("Content management system for personal websites");?></p>
         <?php require_once 'assets/logo.php'?> 
     </div>
         <div class="col-sm-4">
-<h1 class="h3 mb-3 fw-normal">Registracija</h1>
+<h1 class="h3 mb-3 fw-normal"><?php echo t("Registration");?></h1>
 <?php if (isset($error_message)): ?>
     <div class="alert alert-danger" role="alert">
         <?php echo htmlspecialchars($error_message); ?>
@@ -65,40 +65,39 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <?php endif; ?>
 <form method="POST" action="">
     <div class="form-floating">
-        <input type="text" class="form-control" id="username" name="username" placeholder="Vartotojo vardas" required>
-        <label for="username">Vartotojo vardas</label>
+        <input type="text" class="form-control" id="username" name="username" placeholder="<?php echo t("User Name");?>" required>
+        <label for="username"><?php echo t("User Name");?></label>
     </div>
     <br>
     <div class="form-floating">
-        <input type="text" class="form-control" id="surname" name="surname" placeholder="Pavardė" required>
-        <label for="surname">Pavardė</label>
+        <input type="text" class="form-control" id="surname" name="surname" placeholder="<?php echo t("Surname");?>" required>
+        <label for="surname"><?php echo t("Surname");?></label>
     </div>
     <br>
     <div class="form-floating">
-        <input type="text" class="form-control" id="phone" name="phone" placeholder="Telefono numeris" required>
-        <label for="phone">Telefono numeris</label>
+        <input type="text" class="form-control" id="phone" name="phone" placeholder="<?php echo t("Phone");?>" required>
+        <label for="phone"><?php echo t("Phone");?></label>
     </div>
     <br>
     <div class="form-floating">
-        <input type="email" class="form-control" id="email" name="email" placeholder="El. paštas" required>
-        <label for="email">El. paštas</label>
+        <input type="email" class="form-control" id="email" name="email" placeholder="<?php echo t("Email");?>" required>
+        <label for="email"><?php echo t("Email");?></label>
     </div>
     <br>
     <div class="form-floating">
-        <input type="password" class="form-control" id="password" name="password" placeholder="Slaptažodis" required>
-        <label for="password">Slaptažodis</label>
+        <input type="password" class="form-control" id="password" name="password" placeholder="<?php echo t("Password");?>" required>
+        <label for="password"><?php echo t("Password");?></label>
     </div>
     <br>
     <div class="form-floating">
-        <input type="password" class="form-control" id="confirm_password" name="confirm_password" placeholder="Pakartokite slaptažodį" required>
-        <label for="confirm_password">Pakartokite slaptažodį</label>
+        <input type="password" class="form-control" id="confirm_password" name="confirm_password" placeholder="<?php echo t("Repeat the password");?>" required>
+        <label for="confirm_password"><?php echo t("Repeat the password");?></label>
     </div>
     <input type="hidden" name="csrf_token" value="<?php echo generateCSRFToken(); ?>">
     <br>
-    <button class="w-100 btn btn-lg btn-primary" type="submit">Registruotis</button>
+    <button class="w-100 btn btn-lg btn-primary" type="submit"><?php echo t("Register");?></button>
 </form>
 </div>
 </div>
 </div>
    <?php require_once 'core/template/admin-footer.php';?>
-
