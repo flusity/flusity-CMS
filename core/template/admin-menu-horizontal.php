bb21de831feadb091abecf4eb36701f163788546
Darius Jakaitis <105962203+gripas@users.noreply.github.com>
2023-05-26 10:48:09 +0300

    Update admin-menu-horizontal.php

--- core/template/admin-menu-horizontal.php
+++ core/template/admin-menu-horizontal.php
@@ -1,3 +1,7 @@
+<?php   
+  $language_code = getLanguageSetting($db);
+    $translations = getTranslations($db, $language_code);
+?>
 <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top-navbar">
     <a class="navbar-brand ml-5" href="/"><?php require_once $_SERVER['DOCUMENT_ROOT'].'/assets/logo-50.php';?></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -9,7 +13,7 @@
             <li class="nav-item active">
               <?php $settings = getSettings($db); // Funkcija, kuri gauna nustatymus iš duomenų bazės
                 ?>
-                <a class="nav-link" href="<?php echo $settings['pretty_url'] == 1 ? "/" : "../../?page"; ?>">Puslapio priekis</a>
+                <a class="nav-link" href="<?php echo $settings['pretty_url'] == 1 ? "/" : "../../?page"; ?>"><?php echo t("Front page"); ?></a>
             </li>
             <?php if (isset($_SESSION['user_id'])): 
                 $isAdmin = checkUserRole($user_id, 'admin', $db);
@@ -18,28 +22,28 @@
             ?>
         <?php if ($isAdmin || $isModerator): ?>
             <li class="nav-item">
-                <a class="nav-link" href="<?php echo getFullUrl('/admin.php'); ?>">Admin</a>
+                <a class="nav-link" href="<?php echo getFullUrl('/admin.php'); ?>"><?php echo t("Admin"); ?></a>
             </li>
         <?php endif; ?>
     </ul>
     <ul class="navbar-nav mt-2">
         <li class="nav-item">
             <?php if ($isUser): ?>
-                <p style=" margin: 10px 0px -3px 0px;">Sveiki, <?php echo htmlspecialchars($user_name); ?>!</p>
+                <p style=" margin: 10px 0px -3px 0px;"><?php echo t("Hello")." "; ?>, <?php echo htmlspecialchars($user_name); ?>!</p>
             </li>
         <?php endif; ?>
         <li class="nav-item">
-            <a class="nav-link mt-1" href="<?php  $_SERVER['HTTP_HOST']?>/logout.php">Atsijungti</a>
+            <a class="nav-link mt-1" href="<?php  $_SERVER['HTTP_HOST']?>/logout.php"><?php echo t("Log out"); ?></a>
         </li>
         <?php else: ?>
             <li class="nav-item">
-            <a class="nav-link" href="login.php">Log In</a>
+            <a class="nav-link" href="login.php"><?php echo t("Log In"); ?></a>
         </li>
         <li class="nav-item">
             <p class="mt-2">or</p>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="register.php">Sign up</a>
+            <a class="nav-link" href="register.php"><?php echo t("Sign up"); ?></a>
         </li>
         <?php endif; ?>
         </ul>
