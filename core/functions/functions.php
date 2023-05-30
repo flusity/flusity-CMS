22a07461728a70b0dce25b98f2e8be034f5da09d
Darius Jakaitis <105962203+gripas@users.noreply.github.com>
2023-05-30 20:23:14 +0300

    Update functions.php

--- core/functions/functions.php
+++ core/functions/functions.php
@@ -92,15 +92,17 @@ function getDBConnection($config) {
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
     
-    function updateSettings($db, $site_title, $meta_description, $footer_text, $pretty_url, $language) {
-        $stmt = $db->prepare("UPDATE settings SET site_title = :site_title, meta_description = :meta_description, footer_text = :footer_text, pretty_url = :pretty_url, language = :language");
+    function updateSettings($db, $site_title, $meta_description, $footer_text, $pretty_url, $language, $posts_per_page) {
+        $stmt = $db->prepare("UPDATE settings SET site_title = :site_title, meta_description = :meta_description, footer_text = :footer_text, pretty_url = :pretty_url, language = :language, posts_per_page = :posts_per_page");
         $stmt->bindParam(':site_title', $site_title, PDO::PARAM_STR);
         $stmt->bindParam(':meta_description', $meta_description, PDO::PARAM_STR);
         $stmt->bindParam(':footer_text', $footer_text, PDO::PARAM_STR);
         $stmt->bindParam(':pretty_url', $pretty_url, PDO::PARAM_INT);
         $stmt->bindParam(':language', $language, PDO::PARAM_STR);
+        $stmt->bindParam(':posts_per_page', $posts_per_page, PDO::PARAM_INT);
         return $stmt->execute();
     }
+    
     
     
     function createBackupFilename($db) {
