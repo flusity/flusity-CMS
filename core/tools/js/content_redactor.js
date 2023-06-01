07a05f3f0490b6d5dc50cf78f970f265bff02282
Darius Jakaitis <105962203+gripas@users.noreply.github.com>
2023-06-01 19:45:12 +0300

    Update content_redactor.js

--- core/tools/js/content_redactor.js
+++ core/tools/js/content_redactor.js
@@ -1,45 +1,87 @@
 
+
+
 let contentArea = document.getElementById('post_content');
 let displayArea = document.getElementById('display_area');
+
+
+
 
 contentArea.addEventListener('input', function() {
     displayArea.innerHTML = contentArea.value;
 });
 
-function showHTMLContent() {
-    var htmlContent = document.getElementById("post_content").value;
-    document.getElementById("display_area").innerHTML = htmlContent;
+
+function header1() {
+    wrapText('post_content', '<h1>', '</h1>');
+}
+
+function header2() {
+    wrapText('post_content', '<h2>', '</h2>');
+}
+
+function header3() {
+    wrapText('post_content', '<h3>', '</h3>');
+}
+
+function hypertext() {
+    wrapText('post_content', '<a href="#" target="_blank">', '</a>');
 }
 
 function underlineText() {
-    document.execCommand('underline', false, null);
+    wrapText('post_content', '<u>', '</u>');
 }
 
-function boldText() {
-    document.execCommand('bold', false, null);
+function listTextUl() {
+    wrapText('post_content', '<ul>', '</ul>');
 }
 
-function italicText() {
-    document.execCommand('italic', false, null);
+function listTextLi() {
+    wrapText('post_content', '<li>', '</li>');
 }
 
+function quotationMark() {
+    wrapText('post_content', '<q>', '</q>');
+}
 
-function underlineText() {
-    var textarea = document.getElementById("post_content");
-    var selectedText = document.getSelection().toString();
-    var text = textarea.value;
-    var newText = text.replace(selectedText, '<u>' + selectedText + '</u>');
-    textarea.value = newText;
+function markOl() {
+    wrapText('post_content', '<ol>', '</ol>');
+}
+
+function markDel() {
+    wrapText('post_content', '<del>', '</del>');
+}
+function markSub() {
+    wrapText('post_content', '<sub>', '</sub>');
+}
+function markSup() {
+    wrapText('post_content', '<sup>', '</sup>');
+}
+function markHr() {
+    wrapText('post_content', '<hr>', '');
 }
 
 function boldText() {
     wrapText('post_content', '<b>', '</b>');
 }
 
+function markText() {
+    wrapText('post_content', '<mark>', '</mark>');
+}
+
 function italicText() {
     wrapText('post_content', '<i>', '</i>');
 }
 
+function paragraphText() {
+    wrapText('post_content', '<p>', '</p>');
+}
+
+  function selectImage() {
+    var offcanvas = new bootstrap.Offcanvas(document.getElementById('imageSelectOffcanvas'));
+    offcanvas.show();
+  }
+ 
 function wrapText(elementId, openTag, closeTag) {
     var textarea = document.getElementById(elementId);
     if ('selectionStart' in textarea) {
@@ -53,3 +95,4 @@ function wrapText(elementId, openTag, closeTag) {
         textarea.value += openTag + closeTag;
     }
 }
+
