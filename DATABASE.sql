1c5ba0b6d3738e3a53dd4d41d5156d64c280344f
Darius Jakaitis <105962203+gripas@users.noreply.github.com>
2023-06-02 14:05:46 +0300

    Update DATABASE.sql

--- DATABASE.sql
+++ DATABASE.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 02, 2023 at 12:52 PM
+-- Generation Time: Jun 02, 2023 at 01:05 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
