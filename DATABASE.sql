1ae86407fe3d8be35cfac20b3f230f03ac104db2
Darius Jakaitis <105962203+gripas@users.noreply.github.com>
2023-06-02 22:48:11 +0300

    Update DATABASE.sql

--- DATABASE.sql
+++ DATABASE.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 02, 2023 at 02:07 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 7.4.29
+-- Generation Time: Jun 02, 2023 at 09:47 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,7 +31,7 @@ CREATE TABLE `contact_form_settings` (
   `id` int(11) NOT NULL,
   `setting_key` varchar(255) NOT NULL,
   `setting_value` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `contact_form_settings`
@@ -52,7 +52,7 @@ CREATE TABLE `custom_blocks` (
   `menu_id` int(10) UNSIGNED DEFAULT NULL,
   `place_id` int(10) UNSIGNED DEFAULT NULL,
   `html_code` text DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dumping data for table `custom_blocks`
@@ -78,7 +78,7 @@ CREATE TABLE `files` (
   `name` varchar(255) NOT NULL,
   `url` varchar(255) NOT NULL,
   `uploaded_at` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dumping data for table `files`
@@ -105,7 +105,7 @@ CREATE TABLE `menu` (
   `template` varchar(255) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dumping data for table `menu`
@@ -128,7 +128,7 @@ CREATE TABLE `places` (
   `name` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dumping data for table `places`
@@ -163,7 +163,7 @@ CREATE TABLE `posts` (
   `menu_id` int(11) DEFAULT NULL,
   `tags` text DEFAULT NULL,
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `posts`
@@ -179,7 +179,7 @@ INSERT INTO `posts` (`id`, `title`, `content`, `author_id`, `role`, `created_at`
 (28, 'sdfsdf', 'sdfsdf', 2, 'admin', '2023-04-19 18:18:44', 'published', 3, 'aaa', '2023-05-25 09:25:46'),
 (29, 'hh', 'hhhccccccc', 2, 'admin', '2023-04-19 20:56:21', 'draft', 6, 'hddzf', '2023-05-26 16:21:11'),
 (30, 'nnneeeee', 'xcvxerter e er', 2, 'admin', '2023-04-19 21:11:47', 'draft', 3, 'nnnf', '2023-05-26 10:56:16'),
-(31, 'asda', 'adasdasdas', 2, 'admin', '2023-05-26 13:58:04', 'published', 1, 'nnnn', '2023-05-26 16:18:54');
+(31, 'asda', '&lt;b&gt;adasdasdas&lt;/b&gt;', 2, 'admin', '2023-05-26 13:58:04', 'published', 1, 'nnnn', '2023-06-02 14:12:36');
 
 -- --------------------------------------------------------
 
@@ -194,7 +194,7 @@ CREATE TABLE `settings` (
   `pretty_url` int(1) NOT NULL DEFAULT 0,
   `language` varchar(2) NOT NULL DEFAULT 'en',
   `posts_per_page` int(11) NOT NULL DEFAULT 10
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dumping data for table `settings`
@@ -214,7 +214,7 @@ CREATE TABLE `translations` (
   `language_code` varchar(5) NOT NULL,
   `translation_key` varchar(255) NOT NULL,
   `translation_value` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `translations`
@@ -231,7 +231,6 @@ INSERT INTO `translations` (`id`, `language_code`, `translation_key`, `translati
 (28, 'lt', 'Settings successfully updated!', 'Nustatymai sėkmingai atnaujinti!'),
 (29, 'lt', 'Page Name', 'Puslapio pavadinimas'),
 (30, 'lt', 'Settings', 'Nustatymai'),
-(31, 'lt', 'Files', 'Failai'),
 (32, 'lt', 'Contact Form', 'Kontaktų forma'),
 (33, 'lt', 'Posts', 'Įrašai'),
 (34, 'lt', 'Menu', 'Meniu'),
@@ -283,7 +282,6 @@ INSERT INTO `translations` (`id`, `language_code`, `translation_key`, `translati
 (82, 'lt', 'Page URL', 'Puslapio url'),
 (83, 'lt', 'Template', 'Šablonas'),
 (84, 'lt', 'Position', 'Pozicija'),
-(86, 'lt', 'Confirm deletion', 'Patvirtinkite šalinimą'),
 (87, 'lt', 'Are you sure you want to delete this menu?', 'Ar tikrai norite ištrinti šį meniu?'),
 (88, 'lt', 'Menu item successfully added.', 'Meniu punktas sėkmingai pridėtas.'),
 (89, 'lt', 'Error adding menu item. Try again.', 'Klaida pridedant meniu punktą. Bandykite dar kartą.'),
@@ -304,7 +302,6 @@ INSERT INTO `translations` (`id`, `language_code`, `translation_key`, `translati
 (105, 'lt', 'List of backups', 'Atsarginių kopijų sąrašas'),
 (106, 'lt', 'No backups', 'Nėra atsarginių kopijų.'),
 (107, 'lt', 'Custom Blocks', 'Įvairūs Blokai'),
-(109, 'lt', 'Error adding place. Try again.', 'Klaida pridedant Place. Bandykite dar kartą.'),
 (110, 'lt', 'Record successfully added.', 'Įrašas sėkmingai sukurtas.'),
 (111, 'lt', 'Error adding Record. Try again.', 'Pridedant įrašą įvyko klaida. Bandyk iš naujo.'),
 (112, 'lt', 'The record has been updated successfully.', 'Įrašas sėkmingai atnaujintas.'),
@@ -315,7 +312,7 @@ INSERT INTO `translations` (`id`, `language_code`, `translation_key`, `translati
 (118, 'lt', 'Error: Please specify a file name.', 'Klaida: Prašome nurodyti failo pavadinimą.'),
 (120, 'lt', 'Confirm deletion', 'Patvirtinkite šalinimą'),
 (121, 'lt', 'place successfully deleted.', 'Vieta sėkmingai ištrinta.'),
-(122, 'lt', 'Error deleting place. Try again.', 'Klaida trinant Place. Bandykite dar kartą.'),
+(122, 'lt', 'Error deleting place. Try again.', 'Klaida trinant maketo vietą. Bandykite dar kartą.'),
 (123, 'lt', 'Edit Place', 'Redaguoti Vietą'),
 (124, 'lt', 'File not found.', 'Failas nerastas.'),
 (125, 'lt', 'Surname', 'Pavardė'),
@@ -380,7 +377,7 @@ INSERT INTO `translations` (`id`, `language_code`, `translation_key`, `translati
 (184, 'lt', 'Sign out', 'Atsijungti'),
 (185, 'lt', 'Log In', 'Prisijungti'),
 (186, 'lt', 'Sign up', 'Registruotis'),
-(187, 'lt', 'Hello', 'Sveiki'),
+(187, 'lt', 'Hello!', 'Sveiki!'),
 (188, 'lt', 'Front page', 'Puslapio priekis'),
 (189, 'lt', 'Log out', 'Atsijungti'),
 (190, 'lt', 'or', 'arba'),
@@ -398,7 +395,7 @@ INSERT INTO `translations` (`id`, `language_code`, `translation_key`, `translati
 (202, 'lt', 'Error loading file.', 'Įkeliant failą įvyko klaida.'),
 (203, 'lt', 'Failed to clear cache, APCu is not installed!', 'Nepavyko išvalyti talpyklos, APCu neįdiegtas!'),
 (204, 'lt', 'Create', 'Sukurti'),
-(206, 'lt', 'Error adding place. Try again.', 'Klaida pridedant Place. Bandyk iš naujo.'),
+(206, 'lt', 'Error adding place. Try again.', 'Klaida pridedant vietą. Bandyk iš naujo.'),
 (207, 'lt', 'Place successfully added.', 'Vieta sėkmingai pridėta.'),
 (208, 'lt', 'place successfully updated.', 'Vieta sėkmingai atnaujinta.'),
 (209, 'lt', 'place with this name already exists. Try a different name.', 'Vieta tokiu pavadinimu jau yra. Pabandykite kitą pavadinimą.'),
@@ -414,7 +411,11 @@ INSERT INTO `translations` (`id`, `language_code`, `translation_key`, `translati
 (221, 'lt', 'Invalid CSRF token. Try again.', 'Neteisingas CSRF žetonas. Bandykite dar kartą.'),
 (222, 'lt', 'Login Name', 'Prisijungimo Vardas'),
 (223, 'lt', 'Login Name or Email', 'Prisijungimo Vardas / El.paštas'),
-(224, 'lt', 'Add Place', 'Pridėti Vietą');
+(224, 'lt', 'Add Place', 'Pridėti Vietą'),
+(225, 'lt', 'Posts per page', 'Įrašai per puslapį'),
+(226, 'lt', 'Layout Places', 'Maketo Vietos'),
+(227, 'lt', 'Profile', 'Profilis'),
+(228, 'lt', 'User Area', 'Vartotojo aplinka');
 
 -- --------------------------------------------------------
 
@@ -431,7 +432,7 @@ CREATE TABLE `users` (
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
   `role` enum('admin','moderator','user') NOT NULL DEFAULT 'user'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
@@ -547,7 +548,7 @@ ALTER TABLE `posts`
 -- AUTO_INCREMENT for table `translations`
 --
 ALTER TABLE `translations`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=225;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=229;
 
 --
 -- AUTO_INCREMENT for table `users`
