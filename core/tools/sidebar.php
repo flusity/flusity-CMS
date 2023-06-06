ffa3d25e0c2be1d6f17565b1da0c6b94c736ebcd
Darius Jakaitis <105962203+gripas@users.noreply.github.com>
2023-06-06 21:49:05 +0300

    Update sidebar.php
    Signed-off-by: Darius Jakaitis <105962203+gripas@users.noreply.github.com>

--- core/tools/sidebar.php
+++ core/tools/sidebar.php
@@ -5,16 +5,15 @@
     <li class="list-group-item"><a class="nav-link" href="<?php echo getFullUrl('/core/tools/posts.php'); ?>"><i class="fas fa-newspaper"></i> <span class="nav-text"><?php echo t("Posts");?></span></a></li>
     <li class="list-group-item"><a class="nav-link" href="<?php echo getFullUrl('/core/tools/customblock.php'); ?>"><i class="fas fa-shapes"></i> <span class="nav-text"><?php echo t("Block");?></span></a></li>
     <li class="list-group-item"><a class="nav-link" href="<?php echo getFullUrl('/core/tools/files.php'); ?>"><i class="fas fa-folder"></i> <span class="nav-text"><?php echo t("Files");?></span></a></li>
- 
     <li class="list-group-item" id="settingsDropdown">
-      <a class="nav-link"><i class="fas fa-cog"></i> <span class="nav-text"><?php echo t("Core Settings");?></span></a>
+      <a class="nav-link" style="cursor: pointer;"><i class="fas fa-cog"></i> <span class="nav-text"><?php echo t("Core Settings");?></span></a>
       <div id="settingsSubmenu" style="display: none;">
           <a class="nav-link dropdown-item" href="<?php echo getFullUrl('/core/tools/users.php'); ?>"><i class="fas fa-users"></i> <span class="nav-text"><?php echo t("Users");?></span></a>
           <a class="nav-link dropdown-item" href="<?php echo getFullUrl('/core/tools/menu.php'); ?>"><i class="fas fa-bars"></i> <span class="nav-text"><?php echo t("Menu");?></span></a> 
-          <a class="nav-link dropdown-item" href="<?php echo getFullUrl('/core/tools/settings.php'); ?>"><i class="fas fa-cog"></i> <span class="nav-text"><?php echo t("Settings");?></span></a>
           <a class="nav-link dropdown-item" href="<?php echo getFullUrl('/core/tools/language.php'); ?>"><i class="fas fa-language"></i> <span class="nav-text"><?php echo t("Language");?></span></a>
           <a class="nav-link dropdown-item" href="<?php echo getFullUrl('/core/tools/contact_form.php'); ?>"><i class="fas fa-message"></i> <span class="nav-text"><?php echo t("Contact Form");?></span></a>  
           <a class="nav-link dropdown-item" href="<?php echo getFullUrl('/core/tools/places.php'); ?>"><i class="fas fa-tags"></i> <span class="nav-text"><?php echo t("Layout Places");?></span></a>
+          <a class="nav-link dropdown-item" href="<?php echo getFullUrl('/core/tools/settings.php'); ?>"><i class="fas fa-cog"></i> <span class="nav-text"><?php echo t("Settings");?></span></a>
       
         </div>
     </li>
