95fc1ccfe02d3f258e4b436ce3ef00085ffa2f57
Darius Jakaitis <105962203+gripas@users.noreply.github.com>
2023-06-07 15:35:27 +0300

    Add files via upload
    Signed-off-by: Darius Jakaitis <105962203+gripas@users.noreply.github.com>

--- core/template/admin-menu-horizontal.php
+++ core/template/admin-menu-horizontal.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top-navbar">
+<nav class="navbar navbar-expand-lg fixed-top-navbar">
     <a class="navbar-brand ml-5" href="/"><?php require_once $_SERVER['DOCUMENT_ROOT'].'/assets/logo-50.php';?></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -9,7 +9,7 @@
             <li class="nav-item active">
               <?php $settings = getSettings($db); // Funkcija, kuri gauna nustatymus iš duomenų bazės
                 ?>
-                <a class="nav-link" href="<?php echo $settings['pretty_url'] == 1 ? "/" : "../../?page"; ?>"><?php echo t("Front page"); ?></a>
+                <a class="nav-link admin-link-item" href="<?php echo $settings['pretty_url'] == 1 ? "/" : "../../?page"; ?>"><?php echo t("Front page"); ?></a>
             </li>
             <?php if (isset($_SESSION['user_id'])): 
                 $isAdmin = checkUserRole($user_id, 'admin', $db);
@@ -18,7 +18,7 @@
             ?>
         <?php if ($isAdmin || $isModerator): ?>
             <li class="nav-item">
-                <a class="nav-link" href="<?php echo getFullUrl('/admin.php'); ?>"><?php echo t("Dashboard"); ?></a>
+                <a class="nav-link admin-link-item" href="<?php echo getFullUrl('/admin.php'); ?>"><?php echo t("Dashboard"); ?></a>
             </li>
         <?php endif; ?>
     </ul>
@@ -29,17 +29,17 @@
             </li>
         <?php endif; ?>
         <li class="nav-item">
-            <a class="nav-link mt-1" href="<?php  $_SERVER['HTTP_HOST']?>/logout.php"><?php echo t("Log out"); ?></a>
+            <a class="nav-link mt-1 admin-link-item" href="<?php  $_SERVER['HTTP_HOST']?>/logout.php"><?php echo t("Log out"); ?></a>
         </li>
         <?php else: ?>
             <li class="nav-item">
-            <a class="nav-link" href="login.php"><?php echo t("Log In"); ?></a>
+            <a class="nav-link admin-link-item" href="login.php"><?php echo t("Log In"); ?></a>
         </li>
         <li class="nav-item">
             <p class="mt-2">or</p>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="register.php"><?php echo t("Sign up"); ?></a>
+            <a class="nav-link admin-link-item" href="register.php"><?php echo t("Sign up"); ?></a>
         </li>
         <?php endif; ?>
         </ul>
