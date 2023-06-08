22c165d7ebf9e718fb34901661b58195944c9c8f
Darius Jakaitis <105962203+gripas@users.noreply.github.com>
2023-06-08 15:58:26 +0300

    Add files via upload
    Signed-off-by: Darius Jakaitis <105962203+gripas@users.noreply.github.com>

--- get_customblock.php
+++ get_customblock.php
@@ -1,6 +1,6 @@
 <?php
 function getCustomBlocksByUrlNameAndPlace($db, $page_url, $place_name) {
-    $stmt = $db->prepare("SELECT custom_blocks.* FROM custom_blocks JOIN menu ON custom_blocks.menu_id = menu.id JOIN places ON custom_blocks.place_id = places.id WHERE menu.page_url = :page_url AND places.name = :place_name");
+    $stmt = $db->prepare("SELECT v_custom_blocks.* FROM v_custom_blocks JOIN menu ON v_custom_blocks.menu_id = menu.id JOIN places ON v_custom_blocks.place_id = places.id WHERE menu.page_url = :page_url AND places.name = :place_name");
     $stmt->bindParam(':page_url', $page_url, PDO::PARAM_STR);
     $stmt->bindParam(':place_name', $place_name, PDO::PARAM_STR);
     $stmt->execute();
