b9c7c9de4008a0604615b629f47beb4a229b74ef
Darius Jakaitis <105962203+gripas@users.noreply.github.com>
2023-06-08 10:43:54 +0300

    Update content_redactor.js
    Signed-off-by: Darius Jakaitis <105962203+gripas@users.noreply.github.com>

--- core/tools/js/content_redactor.js
+++ core/tools/js/content_redactor.js
@@ -134,7 +134,7 @@ function addImage(imageUrl) {
               left: '0',
               right: '0',
               margin: 'auto',
-              backgroundColor: 'rgba(0, 0, 0, 0.5)',
+              backgroundColor: 'rgba(255, 255, 255, 0.293)',
               color: '#fff',
               height: '100px'
             })
