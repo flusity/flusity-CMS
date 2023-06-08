6831e2b247ca8d8942df02944ed9d556da3e4ca3
Darius Jakaitis <105962203+gripas@users.noreply.github.com>
2023-06-09 01:04:59 +0300

    Add files via upload
    Signed-off-by: Darius Jakaitis <105962203+gripas@users.noreply.github.com>

--- core/tools/settings.php
+++ core/tools/settings.php
@@ -22,9 +22,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     exit;
 }
 ?>
-<?php define('ROOT_PATH', realpath(dirname(__FILE__) . '/../../') . '/');
 
-require_once ROOT_PATH . 'core/template/header-admin.php';?>
 <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/core/template/admin-menu-horizontal.php';?>
   <button class="btn btn-primary position-fixed start-0 translate-middle-y d-md-none tools-settings" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebarOffcanvas" aria-controls="sidebarOffcanvas">
       <i class="fas fa-bars"></i>
