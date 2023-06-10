0f67a0f333385233437df765a0a896c32ebfbfa6
Darius Jakaitis <105962203+gripas@users.noreply.github.com>
2023-06-10 22:57:29 +0300

    Add files via upload
    Signed-off-by: Darius Jakaitis <105962203+gripas@users.noreply.github.com>

--- 404.php
+++ 404.php
@@ -6,53 +6,23 @@
  Author Darius Jakaitis, author web site http://www.manowebas.lt
  fix-content
 */
-require_once 'security/config.php';
-require_once 'core/functions/functions.php';
-require_once 'get_customblock.php';
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-
+require_once 'pre.php';
 $db = getDBConnection($config);
-   // Gaunamas kalbos nustatymas iš duomenų bazės  
+  
     $language_code = getLanguageSetting($db);
     $translations = getTranslations($db, $language_code);
     secureSession($db);
-	
 if (isset($_SESSION['user_id'])) {
     $user_id = $_SESSION['user_id'];
     $user_name = getUserNameById($db, $user_id);
 }
-$settings = getSettings($db);
 
-$limit = $settings['posts_per_page'];
-
-$url = isset($_GET['url']) ? intval($_GET['url']) : 1;
-$offset = ($url - 1) * $limit;
-
-$current_page_url = getCurrentPageUrl($db);
-$posts = getPostsNews($db, $limit, $offset, $current_page_url);
-
-// Dekodavimas
-foreach ($posts as &$post) {
-    $post['title'] = htmlspecialchars_decode($post['title']);
-    $post['content'] = htmlspecialchars_decode($post['content']);
-}
-$total_posts = countPosts($db);
-$total_urls = ceil($total_posts / $limit);
-
-$menu = getMenuByPageUrl($db, $current_page_url);
-
-$templateName = $menu['template'];
-$templatePath = __DIR__ . "/template/{$templateName}.php";
-
-require_once 'template/header.php'; ?>
-
+require_once getThemePath($db, '/template/header.php'); ?>
 
 <header id="header">
-<?php require_once 'template/menu-horizontal.php';?>
+<?php
+require_once getThemePath($db, '/template/menu-horizontal.php'); ?>
+
 </header>
 <section class="container spacer footer">
     <main class="main my-4">
@@ -66,4 +36,5 @@ require_once 'template/header.php'; ?>
 </div>
 </main>
 </section>
-<?php require_once 'template/footer.php';?>
+
+<?php require_once getThemePath($db, '/template/footer.php'); ?>
