9750e9666b561394007016355ed9a39ad8f7555d
Darius Jakaitis <105962203+gripas@users.noreply.github.com>
2023-06-10 21:50:20 +0300

    Delete autoDismiss.js
    Signed-off-by: Darius Jakaitis <105962203+gripas@users.noreply.github.com>

--- assets/main/autoDismiss.js
+++
@@ -1,8 +0,0 @@
-function autoDismissError() {
-    setTimeout(function() {
-        var element = document.getElementById('error_message');
-        if (element) {
-            element.parentNode.removeChild(element);
-        }
-    }, 4000); 
-}
