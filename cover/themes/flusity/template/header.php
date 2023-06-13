ba6fba4e70bf0499471daa504b030112005f017d
Darius Jakaitis <105962203+gripas@users.noreply.github.com>
2023-06-14 00:20:52 +0300

    Add files via upload
    Signed-off-by: Darius Jakaitis <105962203+gripas@users.noreply.github.com>

--- cover/themes/flusity/template/header.php
+++ cover/themes/flusity/template/header.php
@@ -12,6 +12,7 @@ secureSession($db);
 
 $settings = getSettings($db);
 $site_title = isset($settings['site_title']) ? $settings['site_title'] : '';
+$site_brand_icone = isset($settings['brand_icone']) ? $settings['brand_icone'] : '';
 $meta_default_description = isset($settings['meta_description']) ? $settings['meta_description'] : '';
 $meta_default_keywords = isset($settings['default_keywords']) ? $settings['default_keywords'] : '';
 $footer_text = isset($settings['footer_text']) ? $settings['footer_text'] : '';
@@ -36,9 +37,13 @@ if (!empty($postSeo)) {
 <head>
     <meta charset="UTF-8">
     <title><?php echo $site_title;?></title>
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1">   
     <meta name="description" content="<?= isset($meta['description']) ? $meta['description'] : '' ?>">
     <meta name="keywords" content="<?= isset($meta['keywords']) ? $meta['keywords'] : '' ?>">
     <meta http-equiv="Content-Security-Policy: script-src 'self' 'unsafe-inline' https://ssl.gstatic.com 'unsafe-eval'">
+    <link rel="icon" href="uploads/<?php echo $site_brand_icone;?>" type="image/x-icon" />
+    <link rel="shortcut icon" href="uploads/<?php echo $site_brand_icone;?>" type="image/x-icon" />
     <link href="../assets/bootstrap-5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="../assets/dist/js/jquery-3.6.0.min.js"></script>
     <link href="../assets/fonts/fonts-quicksand.css" rel="stylesheet">
