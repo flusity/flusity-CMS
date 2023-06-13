b00aee1c9d8480094d570034f1a5a7dab7e2699c
Darius Jakaitis <105962203+gripas@users.noreply.github.com>
2023-06-14 00:12:19 +0300

    Add files via upload
    Signed-off-by: Darius Jakaitis <105962203+gripas@users.noreply.github.com>

--- core/tools/delete_menu.php
+++ core/tools/delete_menu.php
@@ -9,25 +9,31 @@
     require_once ROOT_PATH . 'security/config.php';
     require_once ROOT_PATH . 'core/functions/functions.php';
  
-    // Duomenų gavimas iš duomenų bazės
     $db = getDBConnection($config); 
 	secureSession($db);
-    // Gaunamas kalbos nustatymas iš duomenų bazės  
     $language_code = getLanguageSetting($db);
     $translations = getTranslations($db, $language_code);
 
     if (defined('IS_ADMIN') && $_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['action']) && $_POST['action'] === 'delete_menu_item' && isset($_POST['menu_item_id'])) {
 
         $menuItemId = intval($_POST['menu_item_id']);
+        $menuItem = getMenuByPageUrl($db, $menuItemId);
+    
+        if ($menuItem['page_url'] === 'index') {
+            $_SESSION['error_message'] = t("The main page cannot be deleted.");
+            echo json_encode($response);
+            exit;
+        }
+    
         $result = deleteMenuItem($db, $menuItemId);
-
+    
         if ($result) {
             $_SESSION['success_message'] = t("Menu item successfully deleted.");
         } else {
             $_SESSION['error_message'] = t("Error deleting menu item. Try again.");
         }
     }
-
+    
     echo json_encode($response);
     exit;
 ?>
