4ae760f583d68fbdeb944e1ee66c531205bebbea
Darius Jakaitis <105962203+gripas@users.noreply.github.com>
2023-06-15 22:32:16 +0300

    Add files via upload
    Signed-off-by: Darius Jakaitis <105962203+gripas@users.noreply.github.com>

--- core/tools/sidebar.php
+++ core/tools/sidebar.php
@@ -23,13 +23,3 @@
     </li>
   </ul>
 </div>
-
-<div class="offcanvas offcanvas-start d-md-none" tabindex="-1" id="sidebarOffcanvas" data-bs-scroll="true" data-bs-backdrop="false">
-    <div class="offcanvas-header">
-        <button type="button" class="btn-close text-reset ms-auto" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-
-    </div>
-    <div class="offcanvas-body" id="sidebarOffcanvasBody">
-    
-    </div>
-</div>
