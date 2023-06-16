4a6ba2dac87d8169deabf2b329390477fb70631a
Darius Jakaitis <105962203+gripas@users.noreply.github.com>
2023-06-16 08:54:27 +0300

    Add files via upload
    Signed-off-by: Darius Jakaitis <105962203+gripas@users.noreply.github.com>

--- core/functions/f_backup.php
+++ core/functions/f_backup.php
@@ -16,56 +16,58 @@ return $filename;
 }
 
 function createDatabaseBackup($db, $backupFilename) {
-$tables = [];
-$result = $db->query("SHOW TABLES");
-while ($row = $result->fetch(PDO::FETCH_NUM)) {
-    $tables[] = $row[0];
-}
-
-$createTableQueries = '';
-$insertIntoTableQueries = '';
-
-foreach ($tables as $table) {
-    $result = $db->query("SELECT * FROM $table");
-    $numFields = $result->columnCount();
-
-    $createTableQueries .= "DROP TABLE IF EXISTS $table;";
-    $row2 = $db->query("SHOW CREATE TABLE $table")->fetch(PDO::FETCH_NUM);
-    $createTableQueries .= "\n\n" . $row2[1] . ";\n\n";
-
-    for ($i = 0; $i < $numFields; $i++) {
-        while ($row = $result->fetch(PDO::FETCH_NUM)) {
-            $insertIntoTableQueries .= "INSERT INTO $table VALUES(";
-            for ($j = 0; $j < $numFields; $j++) {
-                $row[$j] = addslashes($row[$j]);
-                $row[$j] = preg_replace("/\n/", "\\n", $row[$j]);
-                if (isset($row[$j])) {
-                    $insertIntoTableQueries .= '"' . $row[$j] . '"';
-                } else {
-                    $insertIntoTableQueries .= '""';
-                }
-                if ($j < ($numFields - 1)) {
-                    $insertIntoTableQueries .= ',';
-                }
-            }
-            $insertIntoTableQueries .= ");\n";
-        }
+    $tables = [];
+    $result = $db->query("SHOW TABLES");
+    while ($row = $result->fetch(PDO::FETCH_NUM)) {
+        $tables[] = $row[0];
     }
-    $insertIntoTableQueries .= "\n\n\n";
-}
 
-$backupFolder = 'backups/';
-if (!is_dir($backupFolder)) {
-    mkdir($backupFolder, 0777, true);
-}
+    $createTableQueries = '';
+    $insertIntoTableQueries = '';
 
-$backupFileContent = $createTableQueries . $insertIntoTableQueries;
-$backupFilepath = $backupFolder . $backupFilename;
-if (file_put_contents($backupFilepath, $backupFileContent)) {
-    return true;
-} else {
-    return false;
-}
+    foreach ($tables as $table) {
+        $result = $db->query("SELECT * FROM $table");
+        $numFields = $result->columnCount();
+    
+        $createTableQueries .= "DROP TABLE IF EXISTS $table;";
+        $row2 = $db->query("SHOW CREATE TABLE $table")->fetch(PDO::FETCH_NUM);
+        $createTableQueries .= "\n\n" . $row2[1] . ";\n\n";
+    
+        for ($i = 0; $i < $numFields; $i++) {
+            while ($row = $result->fetch(PDO::FETCH_NUM)) {
+                $insertIntoTableQueries .= "INSERT INTO $table VALUES(";
+                for ($j = 0; $j < $numFields; $j++) {
+                    $row[$j] = addslashes($row[$j]);
+                    $row[$j] = preg_replace("/\n/", "\\n", $row[$j]);
+                    if (isset($row[$j]) && $row[$j] !== '') { // check if the value is not empty
+                        $insertIntoTableQueries .= '"' . $row[$j] . '"';
+                    } else {
+                        $insertIntoTableQueries .= 'NULL'; // if the value is empty, insert NULL
+                    }
+                    if ($j < ($numFields - 1)) {
+                        $insertIntoTableQueries .= ',';
+                    }
+                }
+                $insertIntoTableQueries .= ");\n";
+            }
+        }
+        $insertIntoTableQueries .= "\n\n\n";
+    }    
+
+    $backupFolder = 'backups/';
+    if (!is_dir($backupFolder)) {
+        mkdir($backupFolder, 0777, true);
+    }
+
+    // Add SET FOREIGN_KEY_CHECKS statements to the backup file content
+    $backupFileContent = "SET FOREIGN_KEY_CHECKS=0;\n\n" . $createTableQueries . $insertIntoTableQueries . "\n\nSET FOREIGN_KEY_CHECKS=1;";
+
+    $backupFilepath = $backupFolder . $backupFilename;
+    if (file_put_contents($backupFilepath, $backupFileContent)) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 
