93d5ba285b00139430e51c3c3f363e706a226da5
Darius Jakaitis <105962203+gripas@users.noreply.github.com>
2023-06-17 23:19:50 +0300

    Add files via upload
    Signed-off-by: Darius Jakaitis <105962203+gripas@users.noreply.github.com>

---
+++ cover/addons/jd_simple/jd_simple.php
@@ -0,0 +1,6 @@
+<?php
+  $name_addon = "jd_simple";
+  $version = "1v";
+  $author = "JD Flusite";
+  $description_addon ="This test addon";
+?>
---
+++ cover/addons/jd_simple2/jd_simple2.php
@@ -0,0 +1,6 @@
+<?php
+  $name_addon = "jd_simple2";
+  $version = "1.1v";
+  $author = "JD Flusite";
+  $description_addon ="This test addon";
+?>
