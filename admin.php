d32b4a9692e001e1f608f3e732306e83d55bfb2e
Darius Jakaitis <105962203+gripas@users.noreply.github.com>
2023-06-17 23:44:43 +0300

    Add files via upload
    Signed-off-by: Darius Jakaitis <105962203+gripas@users.noreply.github.com>

--- admin.php
+++ admin.php
@@ -27,62 +27,84 @@
             }
         ?>
       </div>
-          <div class="row g-2">
-            <div class="col-4">
-                <div class="p-3 border bg-light">
-                  <?php $places = getplaces($db);
-                  $total_places = count($places);
-                  ?>
-                  <h4><?php echo t("Places");?></h4>
-                  <p><?php echo $total_places; ?></p>
-                </div>
-              </div>
-              <div class="col-4">
-                <div class="p-3 border bg-light">
-                  <?php $allUsers = getAllUsers($db);
-                      $totalUsers = count($allUsers);
-                      ?>
-                <h4><?php echo t("User's");?></h4>
-                  <p><?php echo $totalUsers; ?></p>
-                </div>
-              </div>
-              <div class="col-4">
-                <div class="p-3 border bg-light">
-                <?php $allPosts = getAllPosts($db);
-                      $totalPosts = count($allPosts);
-                      ?>
-                <h4><?php echo t("Post's");?></h4>
-                  <p><?php echo $totalPosts; ?></p>
-                </div>
-              </div>
-              <div class="col-4">
-                <div class="p-3 border bg-light">
-                <?php $allMenu = getMenuItems($db);
-                      $totalMenu = count($allMenu);
-                      ?>
-                <h4><?php echo t("Menu items");?></h4>
-                <p><?php echo $totalMenu; ?></p>
-                </div>
-              </div>
-              <div class="col-4">
-                <div class="p-3 border bg-light">
-                <?php $allBlock = getCustomBlocks($db);
-                      $totalBlock = count($allBlock);
-                      ?>
-                <h4><?php echo t("Block's");?></h4>
-                <p><?php echo $totalBlock; ?></p>
-                </div>
-              </div>
-              <div class="col-4">
-              <div class="p-3 border bg-light">
-                  <?php
-                  $totalFiles = countFilesInDatabase($db);
-                  ?>
-                  <h4><?php echo t("Files");?></h4>
-                  <p><?php echo $totalFiles; ?></p>
-              </div>
-            </div>
-          </div>
+      <div class="row g-2">
+    <div class="col-2">
+        <div class="p-3 border bg-light cms-front-shadow">
+            <i class="fas fa-map-marker-alt fa-2x"></i> <!-- FontAwesome ikona -->
+            <?php 
+            $places = getplaces($db);
+            $total_places = count($places);
+            ?>
+            <h4><?php echo t("Places");?></h4>
+            <p><?php echo $total_places; ?></p>
+        </div>
+    </div>
+    <div class="col-2">
+        <div class="p-3 border bg-light cms-front-shadow">
+            <i class="fas fa-users fa-2x"></i> <!-- FontAwesome ikona -->
+            <?php 
+            $allUsers = getAllUsers($db);
+            $totalUsers = count($allUsers);
+            ?>
+            <h4><?php echo t("User's");?></h4>
+            <p><?php echo $totalUsers; ?></p>
+        </div>
+    </div>
+    <div class="col-2">
+        <div class="p-3 border bg-light cms-front-shadow">
+            <i class="fas fa-newspaper fa-2x"></i> <!-- FontAwesome ikona -->
+            <?php 
+            $allPosts = getAllPosts($db);
+            $totalPosts = count($allPosts);
+            ?>
+            <h4><?php echo t("Post's");?></h4>
+            <p><?php echo $totalPosts; ?></p>
+        </div>
+    </div>
+    <div class="col-2">
+        <div class="p-3 border bg-light cms-front-shadow">
+            <i class="fas fa-bars fa-2x"></i> <!-- FontAwesome ikona -->
+            <?php 
+            $allMenu = getMenuItems($db);
+            $totalMenu = count($allMenu);
+            ?>
+            <h4><?php echo t("Menu");?></h4>
+            <p><?php echo $totalMenu; ?></p>
+        </div>
+    </div>
+    <div class="col-2">
+        <div class="p-3 border bg-light cms-front-shadow">
+            <i class="fas fa-cubes fa-2x"></i> <!-- FontAwesome ikona -->
+            <?php 
+            $allBlock = getCustomBlocks($db);
+            $totalBlock = count($allBlock);
+            ?>
+            <h4><?php echo t("Block's");?></h4>
+            <p><?php echo $totalBlock; ?></p>
+        </div>
+    </div>
+    <div class="col-2">
+        <div class="p-3 border bg-light cms-front-shadow">
+            <i class="fas fa-file fa-2x"></i> <!-- FontAwesome ikona -->
+            <?php 
+            $totalFiles = countFilesInDatabase($db);
+            ?>
+            <h4><?php echo t("Files");?></h4>
+            <p><?php echo $totalFiles; ?></p>
+        </div>
+    </div>
+    <div class="col-2">
+        <div class="p-3 border bg-light cms-front-shadow">
+            <i class="fas fa-puzzle-piece fa-2x"></i> <!-- FontAwesome ikona -->
+            <?php 
+            $totalAddons= totalAddons($db);
+            ?>
+            <h4><?php echo t("Addons");?></h4>
+            <p><?php echo $totalAddons; ?></p>
+        </div>
+    </div>
+</div>
+
       </main>
     </div>
 </div>
