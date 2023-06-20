c4ab7c5b8c4f30318aab78fb8df2083f676399ad
Darius Jakaitis <105962203+gripas@users.noreply.github.com>
2023-06-21 00:26:48 +0300

    Add files via upload
    Signed-off-by: Darius Jakaitis <105962203+gripas@users.noreply.github.com>

--- admin.php
+++ admin.php
@@ -29,78 +29,78 @@
       </div>
       <div class="row g-2">
     <div class="col-2">
-        <div class="p-3 border cms-front-shadow">
-            <i class="fas fa-map-marker-alt fa-2x"></i> <!-- FontAwesome ikona -->
+        <div class="p-3 border cms-front-shadow d-flex gap-2 fs-5">
+            <p><i class="fas fa-map-marker-alt fa-2x"></i> <!-- FontAwesome ikona -->
             <?php 
             $places = getplaces($db, $prefix);
             $total_places = count($places);
             ?>
             <h4><?php echo t("Places");?></h4>
-            <p><?php echo $total_places; ?></p>
+            <?php echo $total_places; ?></p>
         </div>
     </div>
     <div class="col-2">
-        <div class="p-3 border cms-front-shadow">
-            <i class="fas fa-users fa-2x"></i> <!-- FontAwesome ikona -->
+        <div class="p-3 border cms-front-shadow d-flex gap-2 fs-5">
+            <p> <i class="fas fa-users fa-2x"></i> <!-- FontAwesome ikona -->
             <?php 
             $allUsers = getAllUsers($db, $prefix);
             $totalUsers = count($allUsers);
             ?>
-            <h4><?php echo t("User's");?></h4>
-            <p><?php echo $totalUsers; ?></p>
+           <h4><?php echo t("User's");?></h4>
+            <?php echo $totalUsers; ?></p>
         </div>
     </div>
     <div class="col-2">
-        <div class="p-3 border cms-front-shadow">
-            <i class="fas fa-newspaper fa-2x"></i> <!-- FontAwesome ikona -->
+        <div class="p-3 border cms-front-shadow d-flex gap-2 fs-5">
+            <p><i class="fas fa-newspaper fa-2x"></i> <!-- FontAwesome ikona -->
             <?php 
             $allPosts = getAllPosts($db, $prefix);
             $totalPosts = count($allPosts);
             ?>
             <h4><?php echo t("Post's");?></h4>
-            <p><?php echo $totalPosts; ?></p>
+            <?php echo $totalPosts; ?></p>
         </div>
     </div>
     <div class="col-2">
-        <div class="p-3 border cms-front-shadow">
-            <i class="fas fa-bars fa-2x"></i> <!-- FontAwesome ikona -->
+        <div class="p-3 border cms-front-shadow d-flex gap-2 fs-5">
+            <p><i class="fas fa-bars fa-2x"></i> <!-- FontAwesome ikona -->
             <?php 
             $allMenu = getMenuItems($db, $prefix);
             $totalMenu = count($allMenu);
             ?>
             <h4><?php echo t("Menu");?></h4>
-            <p><?php echo $totalMenu; ?></p>
+            <?php echo $totalMenu; ?></p>
         </div>
     </div>
     <div class="col-2">
-        <div class="p-3 border cms-front-shadow">
-            <i class="fas fa-cubes fa-2x"></i> <!-- FontAwesome ikona -->
+        <div class="p-3 border cms-front-shadow d-flex gap-2 fs-5">
+            <p><i class="fas fa-cubes fa-2x"></i> <!-- FontAwesome ikona -->
             <?php 
             $allBlock = getCustomBlocks($db, $prefix);
             $totalBlock = count($allBlock);
             ?>
             <h4><?php echo t("Block's");?></h4>
-            <p><?php echo $totalBlock; ?></p>
+            <?php echo $totalBlock; ?></p>
         </div>
     </div>
     <div class="col-2">
-        <div class="p-3 border cms-front-shadow">
-            <i class="fas fa-file fa-2x"></i> <!-- FontAwesome ikona -->
+        <div class="p-3 border cms-front-shadow d-flex gap-2 fs-5">
+            <p><i class="fas fa-file fa-2x"></i> <!-- FontAwesome ikona -->
             <?php 
             $totalFiles = countFilesInDatabase($db, $prefix);
             ?>
             <h4><?php echo t("Files");?></h4>
-            <p><?php echo $totalFiles; ?></p>
+            <?php echo $totalFiles; ?></p>
         </div>
     </div>
     <div class="col-2">
-        <div class="p-3 border cms-front-shadow">
-            <i class="fas fa-puzzle-piece fa-2x"></i> <!-- FontAwesome ikona -->
+        <div class="p-3 border cms-front-shadow d-flex gap-2 fs-5">
+            <p><i class="fas fa-puzzle-piece fa-2x"></i> <!-- FontAwesome ikona -->
             <?php 
             $totalAddons= totalAddons($db, $prefix);
             ?>
             <h4><?php echo t("Addons");?></h4>
-            <p><?php echo $totalAddons; ?></p>
+            <?php echo $totalAddons; ?></p>
         </div>
     </div>
 </div>
--- get_customblock.php
+++ get_customblock.php
@@ -1,6 +1,6 @@
 <?php
 function getCustomBlocksByUrlNameAndPlace($db, $prefix, $page_url, $place_name) {
-    $stmt = $db->prepare("SELECT ".$prefix['table_prefix']."_v_custom_blocks.* FROM ".$prefix['table_prefix']."_v_custom_blocks JOIN ".$prefix['table_prefix']."_menu ON ".$prefix['table_prefix']."_v_custom_blocks.menu_id = ".$prefix['table_prefix']."_menu.id JOIN ".$prefix['table_prefix']."_places ON ".$prefix['table_prefix']."_v_custom_blocks.place_id = ".$prefix['table_prefix']."_places.id WHERE ".$prefix['table_prefix']."_menu.page_url = :page_url AND ".$prefix['table_prefix']."_places.name = :place_name");
+    $stmt = $db->prepare("SELECT ".$prefix['table_prefix']."_flussi_v_custom_blocks.* FROM ".$prefix['table_prefix']."_flussi_v_custom_blocks JOIN ".$prefix['table_prefix']."_flussi_menu ON ".$prefix['table_prefix']."_flussi_v_custom_blocks.menu_id = ".$prefix['table_prefix']."_flussi_menu.id JOIN ".$prefix['table_prefix']."_flussi_places ON ".$prefix['table_prefix']."_flussi_v_custom_blocks.place_id = ".$prefix['table_prefix']."_flussi_places.id WHERE ".$prefix['table_prefix']."_flussi_menu.page_url = :page_url AND ".$prefix['table_prefix']."_flussi_places.name = :place_name");
     $stmt->bindParam(':page_url', $page_url, PDO::PARAM_STR);
     $stmt->bindParam(':place_name', $place_name, PDO::PARAM_STR);
     $stmt->execute();
