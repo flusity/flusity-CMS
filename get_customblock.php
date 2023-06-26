526870a289b578f75579c541b76a885d2ebaa8b4
Darius Jakaitis <105962203+gripas@users.noreply.github.com>
2023-06-26 11:16:59 +0300

    Add files via upload
    Signed-off-by: Darius Jakaitis <105962203+gripas@users.noreply.github.com>

--- get_customblock.php
+++ get_customblock.php
@@ -1,6 +1,4 @@
 <?php
-
-
 function displayCustomBlockByPlace($db, $prefix, $page_url, $place_name, $admin_label = null) {
 
     $customblocks = getCustomBlocksByUrlNameAndPlace($db, $prefix, $page_url, $place_name);
@@ -15,6 +13,22 @@ function displayCustomBlockByPlace($db, $prefix, $page_url, $place_name, $admin_
         echo '<div>' . $customBlock['html_code'] . '</div>';
         echo '</div>';
     }
+    
 }
+function displayAddon($db, $prefix, $page_url, $place_name, $admin_label = null) {
+    $addon_name="jd_simple_zer";
+    $addons = getAddonsByUrlNameAndPlace($db, $prefix, $addon_name, $page_url, $place_name);
 
+    foreach ($addons as $addon) {
+        echo '<div class="customblock-widget">';
+        if ($admin_label) {
+            echo '<h3>' . htmlspecialchars($admin_label) . '</h3>';
+        } else {
+            echo '<h3>' . htmlspecialchars($addon['title']) . '</h3>';
+        }
+        echo '<div>' . $addon['description'] . '</div>';
+        echo '<div><img src="' . $addon['img_url'] . '" title="' . htmlspecialchars($addon['title']) . '"/></div>';
+        echo '</div>';
+    }
+}
 ?>
