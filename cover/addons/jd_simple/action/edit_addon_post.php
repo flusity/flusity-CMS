2f4fbf41a6a7ade1229f6011d281b897e95d75b8
Darius Jakaitis <105962203+gripas@users.noreply.github.com>
2023-06-26 08:32:13 +0300

    Add files via upload
    Signed-off-by: Darius Jakaitis <105962203+gripas@users.noreply.github.com>

--- cover/addons/jd_simple/action/edit_addon_post.php
+++ cover/addons/jd_simple/action/edit_addon_post.php
@@ -14,13 +14,23 @@ secureSession($db, $prefix);
 $language_code = getLanguageSetting($db, $prefix);
 $translations = getTranslations($db, $prefix, $language_code);
 
+function getAddonById($db, $prefix, $addonId) {
+    $query = 'SELECT * FROM '.$prefix['table_prefix'].'_jd_simple WHERE id = :addonId';
+    $statement = $db->prepare($query);
+    $statement->bindParam(':addonId', $addonId, PDO::PARAM_INT);
+    $statement->execute();
+
+    return $statement->fetch(PDO::FETCH_ASSOC);
+}
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') 
 { 
+    $addonPostId = intval($_POST['addon_post_edit_id']); 
     $id = intval($_POST['id']); 
     
     try {
-        $img_url = null;
-        $img_name = null;
+      //  $img_url = null;
+       // $img_name = null;
         $addonFolder = "jd_simple_img";
 
         if (isset($_FILES['file_id']) && $_FILES['file_id']['error'] == 0) {
@@ -52,22 +62,27 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
         $place_id = $_POST['addon_place_id'];
         $menu_id = $_POST['addon_menu_id'];
 
-        $stmt = $db->prepare("UPDATE " . $prefix['table_prefix'] . "_flussi_jd_simple SET title = :title, description = :description, img_url = :img_url, img_name = :img_name, menu_id = :menu_id, place_id = :place_id WHERE id = :id");
+
+        $stmt = $db->prepare("UPDATE " . $prefix['table_prefix'] . "_jd_simple SET title = :title, description = :description, img_url = :img_url, img_name = :img_name, menu_id = :menu_id, place_id = :place_id WHERE id = :id");
         $stmt->bindParam(':title', $title, PDO::PARAM_STR);
         $stmt->bindParam(':description', $description, PDO::PARAM_STR);
         $stmt->bindParam(':img_url', $img_url, PDO::PARAM_STR);
         $stmt->bindParam(':img_name', $img_name, PDO::PARAM_STR);
         $stmt->bindParam(':menu_id', $menu_id, PDO::PARAM_INT);
         $stmt->bindParam(':place_id', $place_id, PDO::PARAM_INT);
-        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        $stmt->bindParam(':id', $addonPostId, PDO::PARAM_INT);
 
         $stmt->execute();
-        
+        ///$_SESSION['success_message'] = t("success.");
     } catch (Exception $e) {
         $_SESSION['error_message'] = $e->getMessage();
     }
 
-   header('Location: ' . $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/core/tools/addons_model.php?name=jd_simple&id=' . $id);
+    header('Location: ' . $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/core/tools/addons_model.php?name=jd_simple&id='.$id.'&addon_post_edit_id=' . $addonPostId);
 
     exit();
+/*   
+$addon = getAddonById($db, $prefix, $addonPostId );
+var_dump($addon); */
 }
+?>
