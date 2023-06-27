3fbf97c9a21d302b48a8c5f1b69e76ae3f144346
Darius Jakaitis <105962203+gripas@users.noreply.github.com>
2023-06-27 12:49:48 +0300

    Add files via upload
    Signed-off-by: Darius Jakaitis <105962203+gripas@users.noreply.github.com>

--- core/functions/functions.php
+++ core/functions/functions.php
@@ -137,7 +137,39 @@ function getSettings($db, $prefix) {
         return ob_get_clean();
     }
 
-
+    function initializeSystem() {
+        $configurations = require 'security/config.php';
+        require_once 'core/functions/functions.php';
+        $config = $configurations['config'];
+        $prefix = $configurations['prefix'];
+    
+        if (isset($config['display_errors']) && $config['display_errors']) {
+            ini_set('display_errors', 1);
+            ini_set('display_startup_errors', 1);
+            error_reporting(E_ALL);
+        }
+    
+        if (
+            empty($config['db_host']) ||
+            empty($config['db_user']) ||
+            empty($config['db_password']) ||
+            empty($config['db_name']) 
+        ) {
+            header("Location: install/install-flusity.php");
+            exit;
+        }
+    
+        try {
+            $db = getDBConnection($config);
+        } catch (PDOException $e) {
+            error_log('Database connection error: ' . $e->getMessage());
+            header("Location: install/install-flusity.php");
+            exit;
+        }
+    
+        return array($db, $config, $prefix);
+    }
+    
     
     function displayPlace($db, $prefix, $page_url, $place_name, $admin_label = null) {
         // Display custom blocks
