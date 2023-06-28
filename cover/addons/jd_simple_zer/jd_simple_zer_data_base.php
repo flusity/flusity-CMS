e050ac7b05004b73bf2027566447d4f1ecb1eb3e
Darius Jakaitis <105962203+gripas@users.noreply.github.com>
2023-06-28 14:29:10 +0300

    Add files via upload
    Signed-off-by: Darius Jakaitis <105962203+gripas@users.noreply.github.com>

--- cover/addons/jd_simple_zer/jd_simple_zer_data_base.php
+++ cover/addons/jd_simple_zer/jd_simple_zer_data_base.php
@@ -4,7 +4,7 @@ $configurations = require $_SERVER['DOCUMENT_ROOT'] . '/security/config.php';
 // Duomenų gavimas iš duomenų bazės
 $prefix = $configurations['prefix'];
 
-$databaseScript = "CREATE TABLE IF NOT EXISTS {$prefix['table_prefix']}_jd_simple (
+$databaseScript = "CREATE TABLE IF NOT EXISTS {$prefix['table_prefix']}_jd_simple_zer (
     id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     description TEXT,
