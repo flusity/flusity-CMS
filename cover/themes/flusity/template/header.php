97946a41dccd193126123f830dce447046821cf7
Darius Jakaitis <105962203+gripas@users.noreply.github.com>
2023-06-28 11:31:49 +0300

    Add files via upload
    Signed-off-by: Darius Jakaitis <105962203+gripas@users.noreply.github.com>

--- cover/themes/flusity/template/header.php
+++ cover/themes/flusity/template/header.php
@@ -44,13 +44,14 @@ if (!empty($postSeo)) {
     <meta name="description" content="<?= isset($meta['description']) ? $meta['description'] : '' ?>">
     <meta name="keywords" content="<?= isset($meta['keywords']) ? $meta['keywords'] : '' ?>">
     <meta http-equiv="Content-Security-Policy: script-src 'self' 'unsafe-inline' https://ssl.gstatic.com 'unsafe-eval'">
-    <link rel="icon" href="uploads/<?php echo $site_brand_icone;?>" type="image/x-icon" />
-    <link rel="shortcut icon" href="uploads/<?php echo $site_brand_icone;?>" type="image/x-icon" />
-    <link href="../assets/bootstrap-5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="../assets/dist/js/jquery-3.6.0.min.js"></script>
-    <link href="../assets/fonts/fonts-quicksand.css" rel="stylesheet">
-    <link href="<?php echo getThemePath($db, $prefix, 'css/style.css'); ?>" rel="stylesheet">
-    <link href="<?php echo getThemePath($db, $prefix, 'css/site.css'); ?>" rel="stylesheet">
+    <link rel="icon" href="/uploads/<?php echo $site_brand_icone;?>" type="image/x-icon" />
+    <link rel="shortcut icon" href="/uploads/<?php echo $site_brand_icone;?>" type="image/x-icon" />
+    <link rel="stylesheet" type="text/css" href="/assets/bootstrap-5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="/assets/dist/js/jquery-3.6.0.min.js"></script>
+    <link href="/assets/fonts/fonts-quicksand.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="<?php echo getThemePath($db, $prefix, 'css/style.css'); ?>">
+    <link rel="stylesheet" type="text/css"  href="<?php echo getThemePath($db, $prefix, 'css/site.css'); ?>">
+    
    <style>
     .navbar-shrink {
         height: 80px;
