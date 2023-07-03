4f53acaa318e20508cbcdbb79928a86624224821
Darius Jakaitis <105962203+gripas@users.noreply.github.com>
2023-07-04 00:01:41 +0300

    Add files via upload
    Signed-off-by: Darius Jakaitis <105962203+gripas@users.noreply.github.com>

--- core/tools/send_contact_form.php
+++ core/tools/send_contact_form.php
@@ -7,17 +7,18 @@ define('ROOT_PATH', realpath(dirname(__FILE__) . '/../../') . '/');
 require_once ROOT_PATH . 'core/functions/functions.php';
 require_once ROOT_PATH . 'security/config.php';
 
-require_once ROOT_PATH . 'core/PHPMailer/src/PHPMailer.php';
-require_once ROOT_PATH . 'core/PHPMailer/src/SMTP.php';
-require_once ROOT_PATH . 'core/PHPMailer/src/Exception.php';
+require_once ROOT_PATH . 'core/phpmailer/src/PHPMailer.php';
+require_once ROOT_PATH . 'core/phpmailer/src/SMTP.php';
+//require_once ROOT_PATH . 'core/phpmailer/src/POP3.php';
+require_once ROOT_PATH . 'core/phpmailer/src/Exception.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
  $db = getDBConnection($config);
- require_once ROOT_PATH . '../../security/mail_config.php';
+ require_once ROOT_PATH . 'security/mail_config.php';
 
-$mail_config = include(ROOT_PATH . '../../security/mail_config.php');
+$mail_config = include(ROOT_PATH . 'security/mail_config.php');
  
 $mail = new PHPMailer(true);
 
@@ -28,15 +29,22 @@ try {
     $mail->SMTPAuth   = true;
     $mail->Username   = $mail_config['username'];
     $mail->Password   = $mail_config['password'];
-    $mail->SMTPSecure = $mail_config['secure'] === 'tls' ? PHPMailer\PHPMailer\PHPMailer::ENCRYPTION_STARTTLS : PHPMailer\PHPMailer\PHPMailer::ENCRYPTION_SMTPS;
+    $mail->SMTPSecure = $mail_config['secure'];// === 'tls' ? PHPMailer\PHPMailer\PHPMailer::ENCRYPTION_STARTTLS : PHPMailer\PHPMailer\PHPMailer::ENCRYPTION_SMTPS;
     $mail->Port       = $mail_config['port'];
     
-    $mail->setFrom($mail_config['setFrom'], 'Mailer');
+    $mail->setFrom($mail_config['setFrom'], 'Flusity');
     $mail->addAddress($mail_config['addAddress'], 'Admin');   
     
     $mail->isHTML(true);
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->CharSet = 'UTF-8';
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $mail->Subject = 'Naujas pranešimas iš ' . $name;
+    $mail->Body    = 'Gavome pranešimą iš: <b>' . $name . '</b> (' . $email . ')<br><br>' . $message;
+
 
     $mail->send();
 
@@ -47,7 +55,6 @@ try {
     echo json_encode($response);
 
 } catch (Exception $e) {
-    // Jei el. pašto išsiuntimas nepavyko
     $response = [
         'status' => 'error',
         'message' => 'Nepavyko išsiųsti laiško'
