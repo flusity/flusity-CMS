4331a5222558e4451f042aeb652e85d284967c5d
Darius Jakaitis <105962203+gripas@users.noreply.github.com>
2023-07-05 22:46:35 +0300

    Add files via upload
    Signed-off-by: Darius Jakaitis <105962203+gripas@users.noreply.github.com>

--- core/functions/f_users.php
+++ core/functions/f_users.php
@@ -132,6 +132,36 @@ function validateInput($input) {
         return $stmt->execute();
     }
     
+function handleRegister($db, $prefix, $post) {
+    $csrf_token = validateInput($post['csrf_token']);
+    if (!validateCSRFToken($csrf_token)) {
+        return t("Invalid CSRF token. Try again.");
+    } else {
+        $login_name = validateInput($post['login_name']);
+        $username = validateInput($post['username']);
+        $password = validateInput($post['password']);
+        $confirm_password = validateInput($post['confirm_password']);
+        $surname = validateInput($post['surname']);
+        $phone = validateInput($post['phone']);
+        $email = validateInput($post['email']);
+
+        if ($password === $confirm_password) {
+            if (isUsernameTaken($username, $db, $prefix) || isLoginNameTaken($login_name, $db, $prefix)) {
+                return t("That Name or Login Name is already taken. Choose another.");
+            } else {
+                if (registerUser($login_name, $username, $password, $surname, $phone, $email, $db, $prefix)) {
+                    $_SESSION['success_message'] = t("User registration successful. You can now log in.");
+                    header('Location: login.php');
+                    exit();
+                } else {
+                    return t("User registration failed. Try again.");
+                }
+            }
+        } else {
+            return t("Passwords do not match. Try again.");
+        }
+    }
+}
     
     function isLoginNameTaken($login_name, $db, $prefix) {
         $stmt = $db->prepare("SELECT COUNT(*) FROM ".$prefix['table_prefix']."_flussi_users WHERE login_name = :login_name");
