64e470081a65ebdee4a41379275f5f79a4781445
Darius Jakaitis <105962203+gripas@users.noreply.github.com>
2023-07-13 11:31:12 +0300

    Update main.js
    Signed-off-by: Darius Jakaitis <105962203+gripas@users.noreply.github.com>

--- cover/themes/flusity-cms/js/main.js
+++ cover/themes/flusity-cms/js/main.js
@@ -16,30 +16,27 @@ $(window).scroll(function () {
     }
 });
 
-var opacity = 0.382; // pradinė reikšmė
-var direction = 1; // 1 reiškia didėjimą, -1 reiškia mažėjimą
+var opacity = 0.382; 
+var direction = 1; 
 
 setInterval(function() {
-    opacity += direction * 0.001; // keičiame permatomumo reikšmę
+    opacity += direction * 0.001; 
 
-    // jei reikšmė pasiekia 0.5 arba 0.1, keičiame kryptį
     if (opacity >= 0.5) {
         direction = -1;
     } else if (opacity <= 0.1) {
         direction = 1;
     }
 
-    // taikome naująją permatomumo reikšmę
     document.querySelector('.header .overlay').style.backgroundColor = 'rgba(27, 31, 35, ' + opacity + ')';
 
-}, 20); // keičiame kas 20 milisekundes
+}, 20); 
 
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
-var mouseX = window.innerWidth / 2; // pradinė pelės pozicija
-var mouseY = window.innerHeight / 2; // pradinė pelės pozicija
+var mouseX = window.innerWidth / 2; 
+var mouseY = window.innerHeight / 2; 
 
-// Resize canvas to full window size
 function resizeCanvas() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
@@ -47,93 +44,6 @@ function resizeCanvas() {
 resizeCanvas();
 window.onresize = resizeCanvas;
 
-// Generate lines
-
-
-// Draw lines
-/* function drawLines() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.strokeStyle = 'rgba(255, 255, 255, 0.5)'; // white lines with 50% opacity
-  for (var i = 0; i < lines.length; i++) {
-    var line = lines[i];
-    ctx.beginPath();
-    ctx.moveTo(line.x, line.y);
-    ctx.lineTo(line.x + line.length * Math.cos(line.angle), line.y + line.length * Math.sin(line.angle));
-    ctx.stroke();
-    
-    // Move line
-    line.x += line.speed * Math.cos(line.angle);
-    line.y += line.speed * Math.sin(line.angle);
-    
-    // If line is out of bounds, reset it
-    if (line.x < 0 || line.y < 0 || line.x > canvas.width || line.y > canvas.height) {
-      lines[i] = generateLine(mouseX, mouseY);
-    }
-  }
-  
-  requestAnimationFrame(drawLines); // repeat
-} */
-
-var colors = ['rgba(0, 255, 0, 0.15)', 'rgba(0, 0, 255, 0.15)', 'rgba(255, 0, 0, 0.15)', 'rgba(255, 255, 0, 0.15)'];
-
-// Generate lines
-function generateLine(x, y) {
-  var line = {
-    x: x,
-    y: y,
-    length: Math.random() * 500 + 50,
-    angle: Math.random() * 2 * Math.PI,
-    speed: Math.random() * 2 + 1,
-    opacity: Math.random() * 0.5 + 0.5,
-    hasDot: Math.random() < 0.5, // 50% chance a line will have a dot
-    dotColor: colors[Math.floor(Math.random() * colors.length)] // random color
-  };
-  return line;
-}
-var lines = [];
-for (var i = 0; i < 10; i++) { // generate 10 lines
-  lines.push(generateLine(mouseX, mouseY));
-}
-
-// Draw lines
-function drawLines() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.strokeStyle = 'rgba(255, 255, 255, 0.5)'; // white lines with 50% opacity
-  for (var i = 0; i < lines.length; i++) {
-    var line = lines[i];
-    ctx.beginPath();
-    ctx.moveTo(line.x, line.y);
-    ctx.lineTo(line.x + line.length * Math.cos(line.angle), line.y + line.length * Math.sin(line.angle));
-    ctx.stroke();
-
-    // Draw a circle at the end of the line if it should have a dot
-    if (line.hasDot) {
-      var endX = line.x + line.length * Math.cos(line.angle);
-      var endY = line.y + line.length * Math.sin(line.angle);
-      ctx.beginPath();
-      ctx.arc(endX, endY, 5, 0, 2 * Math.PI); // change 5 to the radius you want for the dot
-      ctx.fillStyle = line.dotColor; // color from line
-      ctx.fill();
-    }
-
-    // Move line
-    line.x += line.speed * Math.cos(line.angle);
-    line.y += line.speed * Math.sin(line.angle);
-
-    // If line is out of bounds, reset it
-    if (line.x < 0 || line.y < 0 || line.x > canvas.width || line.y > canvas.height) {
-      lines[i] = generateLine(mouseX, mouseY);
-    }
-  }
-
-  requestAnimationFrame(drawLines); // repeat
-}
-document.addEventListener('mousemove', function(event) {
-  mouseX = event.clientX;
-  mouseY = event.clientY;
-});
-
-drawLines(); 
 
 window.addEventListener('load', (event) => {
   let words = Array.from(document.querySelectorAll('.word'));
@@ -143,7 +53,6 @@ window.addEventListener('load', (event) => {
   });
 });
 
-
 window.addEventListener('load', (event) => {
   let easywords = Array.from(document.querySelectorAll('.easy-word'));
   easywords.forEach((easywords, index) => {
@@ -151,4 +60,3 @@ window.addEventListener('load', (event) => {
     easywords.style.animationDelay = `${index * 0.5}s`;
   });
 });
-
