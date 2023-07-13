d41a463a5660b99996904a829ca8267d22b5893e
Darius Jakaitis <105962203+gripas@users.noreply.github.com>
2023-07-13 15:25:24 +0300

    Add files via upload
    Signed-off-by: Darius Jakaitis <105962203+gripas@users.noreply.github.com>

--- register.php
+++ register.php
@@ -28,73 +28,7 @@
         $error_message = handleRegister($db, $prefix, $_POST);
     }
 ?>
-<header id="header">
-<?php  require_once getThemePath($db, $prefix, '/template/menu-horizontal.php'); ?>
-</header>
-<section class="container spacer footer">
-    <main class="main my-4">
-    <div class="row">
-        <div class="col-sm-4">
-        <h2><?php echo t("Registration system");?></h2>
-        <p><?php echo t("Content management system for personal websites");?></p>
-        <img src="/core/tools/img/flusity-b.png" alt="Flusity logo">
-    </div>
-        <div class="col-sm-4">
-<h1 class="h3 mb-3 fw-normal"><?php echo t("Registration");?></h1>
-<?php if (isset($error_message)): ?>
-    <div id="error_message" class="alert alert-danger" role="alert">
-        <?php echo htmlspecialchars($error_message); ?>
-    </div>
- 
-<?php endif; ?>
-<?php if($registration_enable == 1) { ?>
-<form method="POST" action="">
-    <div class="form-floating">
-        <input type="text" class="form-control" id="login_name" name="login_name" placeholder="<?php echo t("Login Name");?>" required>
-        <label for="login_name"><?php echo t("Login Name");?></label>
-    </div>
-    <br>
-    <div class="form-floating">
-        <input type="text" class="form-control" id="username" name="username" placeholder="<?php echo t("User Name");?>" required>
-        <label for="username"><?php echo t("User Name");?></label>
-    </div>
-    <br>
-    <div class="form-floating">
-        <input type="text" class="form-control" id="surname" name="surname" placeholder="<?php echo t("Surname");?>" required>
-        <label for="surname"><?php echo t("Surname");?></label>
-    </div>
-    <br>
-    <div class="form-floating">
-        <input type="text" class="form-control" id="phone" name="phone" placeholder="<?php echo t("Phone");?>" required>
-        <label for="phone"><?php echo t("Phone");?></label>
-    </div>
-    <br>
-    <div class="form-floating">
-        <input type="email" class="form-control" id="email" name="email" placeholder="<?php echo t("Email");?>" required>
-        <label for="email"><?php echo t("Email");?></label>
-    </div>
-    <br>
-    <div class="form-floating">
-        <input type="password" class="form-control" id="password" name="password" placeholder="<?php echo t("Password");?>" required>
-        <label for="password"><?php echo t("Password");?></label>
-    </div>
-    <br>
-    <div class="form-floating">
-        <input type="password" class="form-control" id="confirm_password" name="confirm_password" placeholder="<?php echo t("Repeat the password");?>" required>
-        <label for="confirm_password"><?php echo t("Repeat the password");?></label>
-    </div>
-    <input type="hidden" name="csrf_token" value="<?php echo generateCSRFToken(); ?>">
-    <br>
-    <button class="w-100 btn btn-lg btn-primary mb-3" type="submit"><?php echo t("Register");?></button>
-</form>
-<?php } else {
-    echo "<p>" . t("Registration is currently suspended. Please try again later") . "</p>";
-}?>
-<p><?php echo t("Back to"); ?>&nbsp;<a href="/" class="btn-link"><?php echo strtolower(t("Home page")); ?></a>&nbsp;<?php echo t("or"); ?>&nbsp;
-&nbsp;<a href="login.php" class="btn-link"><?php echo strtolower(t("Log In")); ?></a>&nbsp;<p>
 
-</div>
-</div>
-</main>
-</section>
+<?php  require_once getThemePath($db, $prefix, '/template/register_form.php'); ?>
+
 <?php  require_once getThemePath($db, $prefix, '/template/footer.php'); ?>
