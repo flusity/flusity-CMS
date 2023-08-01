b6bcf77094a8e7c9c18aecdbc073778a0fdd8ac6
Darius Jakaitis <105962203+gripas@users.noreply.github.com>
2023-08-01 15:03:31 +0300

    Add files via upload
    Signed-off-by: Darius Jakaitis <105962203+gripas@users.noreply.github.com>

--- cover/addons/jd_simple/view.php
+++ cover/addons/jd_simple/view.php
@@ -1,13 +1,17 @@
 <?php
-    foreach ($addons as $addon) {
-        echo '<div class="customblock-widget">';
-        if ($admin_label) {
-            echo '<h3>' . htmlspecialchars($admin_label) . '</h3>';
-        } else {
-            echo '<h3>' . htmlspecialchars($addon['title']) . '</h3>';
-        }
-        echo '<div>' . $addon['description'] . '</div>';
-        echo '<div><img src="' . $addon['img_url'] . '" title="' . htmlspecialchars($addon['title']) . '" width="200px"/></div>';
-        echo '</div>';
+   foreach ($addons as $addon) {
+    echo '<div class="addon-widget">';
+    if ($admin_label) {
+        echo '<h3>' . htmlspecialchars($admin_label) . '</h3>';
+    } else {
+        echo '<h3>' . htmlspecialchars($addon['title']) . '</h3>';
     }
+    echo '<div>' . $addon['description'] . '</div>';
+    echo '<div><img src="' . $addon['img_url'] . '" title="' . htmlspecialchars($addon['title']) . '" width="200px"/></div>';
+
+    displayAddonEditButton($db, $prefix, $addon);
+
+    echo '</div>';
+}
+
 ?>
