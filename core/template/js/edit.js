0d8a2c5600573c7ff8592beafc91aa6adee34cb1
Darius Jakaitis <105962203+gripas@users.noreply.github.com>
2023-08-03 23:24:11 +0300

    Add files via upload
    Signed-off-by: Darius Jakaitis <105962203+gripas@users.noreply.github.com>

---
+++ core/template/js/edit.js
@@ -0,0 +1,19 @@
+
+window.onload = function() {
+    var dropdownBtns = document.querySelectorAll(".add-link");
+    dropdownBtns.forEach(function(btn) {
+        var dropdownMenu = btn.nextElementSibling;
+        btn.addEventListener("click", function() {
+            if (dropdownMenu.style.display === "none") {
+                dropdownMenu.style.display = "block";
+            } else {
+                dropdownMenu.style.display = "none";
+            }
+        });
+    });
+}
+document.querySelectorAll('.myDropdown').forEach(function(dropdown) {
+    dropdown.addEventListener('mouseleave', function() {
+        this.querySelector('.myDropdown-menu').style.display = 'none';
+    });
+});
