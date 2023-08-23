ad39061d1c873767baf31e48d91a96aa36d34d1d
Darius Jakaitis <105962203+gripas@users.noreply.github.com>
2023-08-23 18:54:07 +0300

    Add files via upload
    Signed-off-by: Darius Jakaitis <105962203+gripas@users.noreply.github.com>

---
+++ cover/themes/free-time/translator.php
@@ -0,0 +1,36 @@
+<?php
+
+$selected_lang = isset($_SESSION['lang']) ? $_SESSION['lang'] : $lang_code;
+
+$translations_lt = array(
+    'contact_us' => 'Susisiekite su mumis',
+    'interesting_theme' => 'Dominanti tema',
+    'your_email' => 'Jūsų el. paštas',
+    'message' => 'Žinutė',
+    'human_check' => 'Patikrinama ar esate žmogus',
+    'drag_answer' => 'Tempkite atsakymą čia',
+    'send' => 'Siųsti',
+    'dashboard' => 'Prietaisų skydelis',
+    'profile' => 'Profilis',
+    'signout' => 'Atsijungti',
+    '404_tropic'=> 'Jūs nuklydote į tropinę nežinią! Nieko jūs čia nepamatysite.',
+    '404_return_home'=>'Spustelėkite <a href="/">čia</a>, kad grįžtumėte į pagrindinį puslapį.'
+);
+
+$translations_en = array(
+    'contact_us' => 'Contact Us',
+    'interesting_theme' => 'Interesting Theme',
+    'your_email' => 'Your Email',
+    'message' => 'Message',
+    'human_check' => 'Human Check',
+    'drag_answer' => 'drag the answer here',
+    'send' => 'Send',
+    'dashboard' => 'Dashboard',
+    'profile' => 'Profile',
+    'signout' => 'Sign out',
+    '404_tropic'=> 'You\'ve wandered off into tropical limbo! You won\'t see anything here.',
+    '404_return_home'=>'Click <a href="/">here</a>, to return back home page.'
+);
+
+$translations = ($selected_lang == 'lt') ? $translations_lt : $translations_en;
+?>
