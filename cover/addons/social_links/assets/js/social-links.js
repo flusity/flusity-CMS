e72ac5f6da0b475694e078ee7dd3572b935bf53b
Darius Jakaitis <105962203+gripas@users.noreply.github.com>
2023-08-27 23:40:11 +0300

    Add files via upload
    Signed-off-by: Darius Jakaitis <105962203+gripas@users.noreply.github.com>

--- cover/addons/social_links/assets/js/social-links.js
+++ cover/addons/social_links/assets/js/social-links.js
@@ -3,4 +3,4 @@
 * Indicates in the JS files where to present the libraries in the document.
 * Important! indicate where to broadcast the head or footer, necessarily between the comments
 */
-/*head*/
+/*footer*/
