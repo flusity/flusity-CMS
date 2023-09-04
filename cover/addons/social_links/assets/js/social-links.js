9c20eac7aad85e83d66cb52463e443e09f74315d
Darius Jakaitis <105962203+flusity@users.noreply.github.com>
2023-09-04 21:24:06 +0300

    Delete cover/addons/social_links directory
    Signed-off-by: Darius Jakaitis <105962203+flusity@users.noreply.github.com>

--- cover/addons/social_links/assets/js/social-links.js
+++
@@ -1,6 +0,0 @@
-
-/* 
-* Indicates in the JS files where to present the libraries in the document.
-* Important! indicate where to broadcast the head or footer, necessarily between the comments
-*/
-/*footer*/
--- cover/addons/social_links/social_links.php
+++
@@ -1,6 +0,0 @@
-<?php
-  $name_addon = "social_links";
-  $version = "1v";
-  $author = "JD Flusite";
-  $description_addon ="Social links addon";
-?>
