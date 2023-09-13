902338bc951eac81740801268d14baf0f25e1bba
Darius Jakaitis <105962203+flusity@users.noreply.github.com>
2023-09-13 22:37:41 +0300

    Add files via upload
    Signed-off-by: Darius Jakaitis <105962203+flusity@users.noreply.github.com>

--- cover/addons/info_media_gallery/core/media_function.php
+++ cover/addons/info_media_gallery/core/media_function.php
@@ -1,4 +1,35 @@
 <?php
+function fetchGalleryItems($db, $prefix, $addon, $current_lang) {
+    $result = [];
+
+    $stmt = $db->prepare("SELECT * FROM  ". $prefix['table_prefix'] ."_info_media_gallery WHERE id = :addon_id");
+    $stmt->bindParam(':addon_id', $addon['id'], PDO::PARAM_INT);
+    $stmt->execute();
+    $galleries = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($galleries as $gallery) {
+        $stmt = $db->prepare("SELECT * FROM ". $prefix['table_prefix'] ."_info_media_gallery_item WHERE id_info_media_gallery = :gallery_id");
+        $stmt->bindParam(':gallery_id', $gallery['id'], PDO::PARAM_INT);
+        $stmt->execute();
+        $items = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach ($items as $item) {
+            $image_id = $item['media_file_id'];  
+            $galleryTitle = ($current_lang == 'en' && !empty($item['lang_en_title'])) ? $item['lang_en_title'] : $item['title'];
+            $galleryInfoDesc = ($current_lang == 'en' && !empty($item['lang_en_media_description'])) ? $item['lang_en_media_description'] : $item['media_description'];
+
+            $file_stmt = $db->prepare("SELECT url FROM " . $prefix['table_prefix'] . "_flussi_files WHERE id = :file_id");
+            $file_stmt->bindParam(':file_id', $image_id, PDO::PARAM_INT);
+            $file_stmt->execute();
+            $file_data = $file_stmt->fetch(PDO::FETCH_ASSOC);
+            $file_url = $file_data['url'] ?? ''; 
+
+            $result[] = ['title' => $galleryTitle, 'desc' => $galleryInfoDesc, 'url' => $file_url];
+        }
+    }
+
+    return $result;
+}
 
 function getMediaItemsByAddonId($db, $prefix, $addonId) {
     $stmt = $db->prepare("SELECT * FROM " . $prefix['table_prefix'] . "_info_media_gallery_item WHERE id_info_media_gallery = :id");
--- cover/addons/info_media_gallery/view.php
+++ cover/addons/info_media_gallery/view.php
@@ -1,43 +1,23 @@
 <?php
+require $_SERVER['DOCUMENT_ROOT'] . '/cover/addons/info_media_gallery/core/media_function.php';
 $settings = getSettings($db, $prefix);
-$lang_code = $settings['language']; // Kalbos kodas
+$lang_code = $settings['language'];
 $current_lang = $_SESSION['lang'] ?? $lang_code;
-
 $class = (isset($_SESSION['user_role']) && $_SESSION['user_role'] == 'admin') ? 'highlight-drag' : '';
-
-$stmt = $db->prepare("SELECT * FROM  ". $prefix['table_prefix'] ."_info_media_gallery WHERE id = :addon_id");
-$stmt->bindParam(':addon_id', $addon['id'], PDO::PARAM_INT);
-$stmt->execute();
-$galleries = $stmt->fetchAll(PDO::FETCH_ASSOC);
 $galleryStyleCss = $addon['gallery_css_style_settings'];
-
-    echo '<div id="styleCss" class="gallery-container d-flex ' . $class . '" data-style-css="'. $galleryStyleCss .'">';
-    if (isset($admin_label)) {
-        echo '<h3>' . htmlspecialchars($admin_label) . '</h3>';
-    }
-    foreach ($galleries as $gallery) {
-
-        $stmt = $db->prepare("SELECT * FROM ". $prefix['table_prefix'] ."_info_media_gallery_item WHERE id_info_media_gallery = :gallery_id");
-        $stmt->bindParam(':gallery_id', $gallery['id'], PDO::PARAM_INT);
-        $stmt->execute();
-        $items = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-        foreach ($items as $item) {
-        $image_id = $item['media_file_id'];  
-        $galleryTitle = ($current_lang == 'en' && !empty($item['lang_en_title'])) ? $item['lang_en_title'] : $item['title'];
-        $galleryInfoDesc = ($current_lang == 'en' && !empty($item['lang_en_media_description'])) ? $item['lang_en_media_description'] : $item['media_description'];
-
-            $file_stmt = $db->prepare("SELECT url FROM " . $prefix['table_prefix'] . "_flussi_files WHERE id = :file_id");
-            $file_stmt->bindParam(':file_id', $image_id, PDO::PARAM_INT);
-            $file_stmt->execute();
-            $file_data = $file_stmt->fetch(PDO::FETCH_ASSOC);
-            $file_url = $file_data['url'] ?? ''; 
-
-            echo '<div class="image-card" data-desc="' . htmlspecialchars($galleryInfoDesc) . '" style="width: calc(50% - 20px);">';
-        echo '<img src="' . $file_url . '" alt="' . htmlspecialchars($galleryTitle). '">';
-            echo '</div>';
-        }
-  } 
-    displayAddonEditButton($db, $prefix, $addon, 'info_media_gallery');
-    echo '</div>';
+$items = fetchGalleryItems($db, $prefix, $addon, $current_lang);
 ?>
+
+<div id="styleCss" class="gallery-container d-flex <?= $class ?>" data-style-css="<?= $galleryStyleCss ?>">
+    <?php if (isset($admin_label)): ?>
+        <h3><?= htmlspecialchars($admin_label) ?></h3>
+    <?php endif; ?>
+
+    <?php foreach ($items as $item): ?>
+        <div class="image-card" data-desc="<?= htmlspecialchars($item['desc']) ?>" style="width: calc(50% - 20px);">
+            <img src="<?= $item['url'] ?>" alt="<?= htmlspecialchars($item['title']) ?>">
+        </div>
+    <?php endforeach; ?>
+    
+    <?php displayAddonEditButton($db, $prefix, $addon, 'info_media_gallery'); ?>
+</div>
