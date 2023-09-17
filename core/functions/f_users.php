f77fb4f9e5237c233fb881e8876a0fa4fc23168a
Darius Jakaitis <105962203+flusity@users.noreply.github.com>
2023-09-17 09:07:20 +0300

    Add files via upload
    Signed-off-by: Darius Jakaitis <105962203+flusity@users.noreply.github.com>

--- core/functions/f_users.php
+++ core/functions/f_users.php
@@ -117,18 +117,20 @@
             session_start();
         }
     
+        if (!isset($_SESSION['user_id'])) {
+            return;
+        }
+    
         if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity'] > $inactive)) {
     
-            // Įsimename norimą puslapį prieš sesiją užbaigiant
             $requested_url = $_SERVER['REQUEST_URI'];
     
             session_unset();
             session_destroy();
-            session_start();  // Pradėkite naują sesiją, kad galėtumėte įrašyti norimą URL
+            session_start();  
     
             $_SESSION['requested_url'] = $requested_url;
     
-            // Nukreipiamas į prisijungimo puslapį
             $redirect_login = $base_url . "/login"; 
             header("Location: " . $redirect_login);
             exit;
@@ -136,6 +138,7 @@
     
         $_SESSION['last_activity'] = time();
     }
+    
     
 
     function authenticateUser($login_nameOrEmail, $password, $prefix) {
