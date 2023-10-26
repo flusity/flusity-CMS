8214c45168e110d726a6c8cc8def5098f2658338
Darius Jakaitis <105962203+flusity@users.noreply.github.com>
2023-10-26 22:43:18 +0300

    Add files via upload
    Signed-off-by: Darius Jakaitis <105962203+flusity@users.noreply.github.com>

--- cover/addons/event_callendar/model_tools.php
+++ cover/addons/event_callendar/model_tools.php
@@ -217,25 +217,38 @@ if ($mode === 'create' || $addon) { ?>
     $stmt = $db->prepare("SELECT * FROM " . $prefix['table_prefix'] . "_event_callendar_holidays");
     $stmt->execute();
     $holidays = $stmt->fetchAll(PDO::FETCH_ASSOC);
-?>        
-            
+ 
+$edit_holiday_data = null;
+if (isset($_GET['edit_holiday_id'])) {
+    $edit_holiday_id = intval($_GET['edit_holiday_id']);
+    $stmt = $db->prepare("SELECT * FROM " . $prefix['table_prefix'] . "_event_callendar_holidays WHERE id = :edit_holiday_id");
+    $stmt->bindParam(':edit_holiday_id', $edit_holiday_id, PDO::PARAM_INT);
+    $stmt->execute();
+    $edit_holiday_data = $stmt->fetch(PDO::FETCH_ASSOC);
+}
+?>
+
                 <h3><?php echo t('Create Holiday'); ?></h3>
-                <form action="add_holiday.php" method="post">             
-                    <select name="month">
-                        <option value="" disabled selected><?php echo t('Month'); ?></option>
-                        <?php foreach ($monthNames as $key => $value): ?>
-                            <option value="<?php echo $key; ?>"><?php echo t($value); ?></option>
-                        <?php endforeach; ?>
-                    </select>
-                                            
-                    <select name="holiday">
-                        <option value="" disabled selected><?php echo t('Day'); ?></option>
-                        <?php for($i=1; $i<=31; $i++){ ?>
-                            <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
-                        <?php } ?>
-                    </select>
-                    <input type="text" name="holiday_name" placeholder="Holiday name">                         
-                    <input type="submit" value="<?php echo t('Add Holiday'); ?>">
+                <form action="../../cover/addons/event_callendar/action/add_holiday.php" method="post">
+                    
+                
+                <input type="hidden" name="edit_holiday_id" value="<?php echo ($edit_holiday_data !== null) ? $edit_holiday_data['id'] : ''; ?>">      
+                <input type="hidden" name="id" value="<?php echo $id; ?>">       
+                <select name="month">
+                    <option value="" disabled <?php echo ($edit_holiday_data === null) ? 'selected' : ''; ?>><?php echo t('Month'); ?></option>
+                    <?php foreach ($monthNames as $key => $value): ?>
+                        <option value="<?php echo $key; ?>" <?php echo ($edit_holiday_data !== null && $edit_holiday_data['month'] == $key) ? 'selected' : ''; ?>><?php echo t($value); ?></option>
+                    <?php endforeach; ?>
+                </select>
+
+                <select name="holiday">
+                    <option value="" disabled <?php echo ($edit_holiday_data === null) ? 'selected' : ''; ?>><?php echo t('Day'); ?></option>
+                    <?php for($i=1; $i<=31; $i++){ ?>
+                        <option value="<?php echo $i; ?>" <?php echo ($edit_holiday_data !== null && $edit_holiday_data['holiday'] == $i) ? 'selected' : ''; ?>><?php echo $i; ?></option>
+                    <?php } ?>
+                </select>
+                <input type="text" name="holiday_name" placeholder="Holiday name" value="<?php echo ($edit_holiday_data !== null) ? htmlspecialchars($edit_holiday_data['holiday_name']) : ''; ?>">                       
+                <input type="submit" value="<?php echo t('Add or Edit Holiday'); ?>">
                 </form>
 
 
@@ -260,10 +273,11 @@ if ($mode === 'create' || $addon) { ?>
                     <td><?php echo htmlspecialchars($holiday['holiday']); ?></td>
                     <td><?php echo htmlspecialchars($holiday['holiday_name']); ?></td>
                     <td><?php 
-                    echo '<a href="addons_model.php?name=event_callendar&id=' . htmlspecialchars($_GET['id']) . '&addon_event_edit_id=' . htmlspecialchars($addonRes['id']) . '"><i class="fa fa-edit"></i></a> ';
-                    echo '<a href="../../cover/addons/event_callendar/action/delete_addon_post.php?name=event_callendar&id=' . htmlspecialchars($_GET['id']) . '&addon_event_id=' . htmlspecialchars($addonRes['id']) . '"><i class="fa fa-trash"></i></a>';
+                    echo '<a href="addons_model.php?name=event_callendar&id='. htmlspecialchars($_GET['id']).'&edit_holiday_id='. htmlspecialchars($holiday['id']).'"><i class="fa fa-edit"></i></a>';        
+                    echo '<a href="../../cover/addons/event_callendar/action/delete_holiday.php?event_calendar='.$id.'&holiday_id='. htmlspecialchars($holiday['id']) .'"><i class="fa fa-trash"></i></a>';
                     
                     ?></td>
+                    
                 </tr>
         <?php $i++; 
             endforeach; ?>
@@ -290,7 +304,7 @@ if ($mode === 'create' || $addon) { ?>
                         <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
                         <div class="accordion-body">
                       
-                            <form action="add_event_manager.php" method="post">
+                            <form action="../../cover/addons/event_callendar/action/add_event_manager.php" method="post">
                                 <input type="text" name="event_name" placeholder="Event Name">
                                 <input type="date" name="when_event_will_start" placeholder="When Event Will Start" value="<?php echo date('Y-m-d'); ?>">
                                 <input type="text" name="event_days" placeholder="Event Days">
