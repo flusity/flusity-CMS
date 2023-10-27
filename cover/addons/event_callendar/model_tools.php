8777626e6b950d3d7357c419d6acef87ebd6a8d2
Darius Jakaitis <105962203+flusity@users.noreply.github.com>
2023-10-27 23:36:41 +0300

    Add files via upload
    Signed-off-by: Darius Jakaitis <105962203+flusity@users.noreply.github.com>

--- cover/addons/event_callendar/model_tools.php
+++ cover/addons/event_callendar/model_tools.php
@@ -290,7 +290,7 @@ if (isset($_GET['edit_holiday_id'])) {
     </div>
 </div>
           
-            <div class="tab-pane fade" id="coordinators">
+            <div class="tab-pane fade" id="coordinators" data-tab="coordinators">
               <div class="form-group row p-2">
                 <h3>
                     <?php echo t('Cabinet heads'); ?>
@@ -303,9 +303,21 @@ if (isset($_GET['edit_holiday_id'])) {
                         </h2>
                         <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
                         <div class="accordion-body">
-                      
+                            <?php
+                            $editId = isset($_GET['event_edit_id']) ? intval($_GET['event_edit_id']) : null;
+                            if ($editId !== null) {
+                                $stmt = $db->prepare("SELECT * FROM " . $prefix['table_prefix'] . "_event_callendar_laboratories WHERE id = :id");
+                                $stmt->bindParam(':id', $editId, PDO::PARAM_INT);
+                                $stmt->execute();
+                                $eventToEdit = $stmt->fetch(PDO::FETCH_ASSOC);
+                                
+                            }
+                            ?>
+
                             <form action="../../cover/addons/event_callendar/action/add_event_manager.php" method="post">
+                            <input type="hidden" name="action" value="<?php echo isset($eventToEdit) ? 'edit' : 'add'; ?>">
                             <input type="hidden" name="id" value="<?php echo $id; ?>">
+                            <input type="hidden" name="event_edit_id" value="<?php echo $editId; ?>">
                             <select name="callendar_id" id="callendarSelect" data-placeholder="<?php echo t('Select Calendar...'); ?>">
                                 <?php
                                 $stmt = $db->prepare("SELECT id, callendar_name FROM " . $prefix['table_prefix'] . "_event_callendar");
@@ -317,46 +329,48 @@ if (isset($_GET['edit_holiday_id'])) {
                                 ?>
                             </select>
  
-                                <input type="text" name="event_name" placeholder="Event Name">
-                                <input type="date" name="when_event_will_start" placeholder="When Event Will Start" value="<?php echo date('Y-m-d'); ?>">
-                                <input type="text" name="event_days" placeholder="Event Days">
-                                                
+                            <input type="text" name="event_name" placeholder="Event Name" value="<?php echo isset($eventToEdit) ? htmlspecialchars($eventToEdit['event_name']) : ''; ?>">
+                            <input type="date" name="when_event_will_start" placeholder="When Event Will Start" value="<?php echo isset($eventToEdit) ? htmlspecialchars($eventToEdit['when_event_will_start']) : date('Y-m-d'); ?>">
+                            <input type="text" name="event_days" placeholder="Event Days" value="<?php echo isset($eventToEdit) ? htmlspecialchars($eventToEdit['event_days']) : ''; ?>">
+                                            
                                 <select name="event_color">
-                                    <option value="" disabled selected><?php echo t('Color'); ?></option>
-                                    <option value="red">Red</option>
-                                    <option value="green">Green</option>
-                                    <option value="blue">Blue</option>
-                                    <option value="yellow">Yellow</option>
-                                    <option value="orange">Orange</option>
-                                    <option value="purple">Purple</option>
-                                    <option value="pink">Pink</option>
-                                    <option value="brown">Brown</option>
-                                    <option value="teal">Teal</option>
-                                    <option value="cyan">Cyan</option>
-                                    <option value="magenta">Magenta</option>
-                                    <option value="gold">Gold</option>
-                                    <option value="silver">Silver</option>
-                                    <option value="navy">Navy</option>
-                                    <option value="lime">Lime</option>
-                                    <option value="indigo">Indigo</option>
-                                    <option value="beige">Beige</option>
-                                    <option value="maroon">Maroon</option>
-                                    <option value="olive">Olive</option>
+                                    <option value="" disabled <?php echo !isset($eventToEdit) ? 'selected' : ''; ?>><?php echo t('Color'); ?></option>
+                                    <option value="red" <?php echo isset($eventToEdit) && $eventToEdit['event_color'] === 'red' ? 'selected' : ''; ?>><?php echo t('Red'); ?></option> <!-- 337 eilutė -->
+                                    <option value="green" <?php echo isset($eventToEdit) && $eventToEdit['event_color'] === 'green' ? 'selected' : ''; ?>><?php echo t('Green'); ?></option>
+                                    <option value="blue" <?php echo isset($eventToEdit) && $eventToEdit['event_color'] === 'blue' ? 'selected' : ''; ?>><?php echo t('Blue'); ?></option>
+                                    <option value="yellow" <?php echo isset($eventToEdit) && $eventToEdit['event_color'] === 'yellow' ? 'selected' : ''; ?>><?php echo t('Yellow'); ?></option>
+                                    <option value="orange" <?php echo isset($eventToEdit) && $eventToEdit['event_color'] === 'orange' ? 'selected' : ''; ?>><?php echo t('Orange'); ?></option>
+                                    <option value="purple" <?php echo isset($eventToEdit) && $eventToEdit['event_color'] === 'purple' ? 'selected' : ''; ?>><?php echo t('Purple'); ?></option>
+                                    <option value="pink" <?php echo isset($eventToEdit) && $eventToEdit['event_color'] === 'pink' ? 'selected' : ''; ?>><?php echo t('Pink'); ?></option>
+                                    <option value="brown" <?php echo isset($eventToEdit) && $eventToEdit['event_color'] === 'brown' ? 'selected' : ''; ?>><?php echo t('Brown'); ?></option>
+                                    <option value="teal" <?php echo isset($eventToEdit) && $eventToEdit['event_color'] === 'teal' ? 'selected' : ''; ?>><?php echo t('Teal'); ?></option>
+                                    <option value="cyan" <?php echo isset($eventToEdit) && $eventToEdit['event_color'] === 'cyan' ? 'selected' : ''; ?>><?php echo t('Cyan'); ?></option>
+                                    <option value="magenta" <?php echo isset($eventToEdit) && $eventToEdit['event_color'] === 'magenta' ? 'selected' : ''; ?>><?php echo t('Magenta'); ?></option>
+                                    <option value="gold" <?php echo isset($eventToEdit) && $eventToEdit['event_color'] === 'gold' ? 'selected' : ''; ?>><?php echo t('Gold'); ?></option>
+                                    <option value="silver" <?php echo isset($eventToEdit) && $eventToEdit['event_color'] === 'silver' ? 'selected' : ''; ?>><?php echo t('Silver'); ?></option>
+                                    <option value="navy" <?php echo isset($eventToEdit) && $eventToEdit['event_color'] === 'navy' ? 'selected' : ''; ?>><?php echo t('Navy'); ?></option>
+                                    <option value="lime" <?php echo isset($eventToEdit) && $eventToEdit['event_color'] === 'lime' ? 'selected' : ''; ?>><?php echo t('Lime'); ?></option>
+                                    <option value="indigo" <?php echo isset($eventToEdit) && $eventToEdit['event_color'] === 'indigo' ? 'selected' : ''; ?>><?php echo t('Indigo'); ?></option>
+                                    <option value="beige" <?php echo isset($eventToEdit) && $eventToEdit['event_color'] === 'beige' ? 'selected' : ''; ?>><?php echo t('Beige'); ?></option>
+                                    <option value="maroon" <?php echo isset($eventToEdit) && $eventToEdit['event_color'] === 'maroon' ? 'selected' : ''; ?>><?php echo t('Maroon'); ?></option>
+                                    <option value="olive" <?php echo isset($eventToEdit) && $eventToEdit['event_color'] === 'olive' ? 'selected' : ''; ?>><?php echo t('Olive'); ?></option>
                                 </select>
                                 
-                                <select name="new_manager_id[]" multiple="multiple" id="managerSelect" data-placeholder="<?php echo t('Select Managers...'); ?>">
-                                    
-                                    <?php
-                                    $stmt = $db->prepare("SELECT id, username FROM " . $prefix['table_prefix'] . "_flussi_users WHERE role IN ('admin', 'moderator')");
-                                    $stmt->execute();
-                                    $managers = $stmt->fetchAll(PDO::FETCH_ASSOC);
-                                    foreach ($managers as $manager) {
-                                        echo '<option value="' . $manager['id'] . '">' . $manager['username'] . '</option>';
-                                    }
-                                    ?>
-                                </select>
+                                <select name="new_manager_id[]" multiple="multiple" id="managerSelect"   data-placeholder="<?php echo t('Select Managers...'); ?>">
+                                <?php
+                                $stmt = $db->prepare("SELECT id, username FROM " . $prefix['table_prefix'] . "_flussi_users WHERE role IN ('admin', 'moderator')");
+                                $stmt->execute();
+                                $managers = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                                foreach ($managers as $manager) {
+                                    $selected = isset($eventToEdit) && in_array($manager['id'], explode(',', $eventToEdit['managers'])) ? 'selected' : '';
+                                    echo '<option value="' . $manager['id'] . '" ' . $selected . '>' . $manager['username'] . '</option>';
+                                }
+                                ?>
+                            </select>
                                 
-                                <input type="submit" value="<?php echo t('Add Event & Managers'); ?>">
+                            <input type="submit" value="<?php echo t('Add/Edit Event & Managers'); ?>">
+                            <a href="addons_model.php?name=event_callendar&id=<?php echo htmlspecialchars($_GET['id']) ?>" class="btn btn-secondary"><?php echo t('Cancel');?></a>
+
                             </form>
                         </div>
                         </div>
@@ -432,7 +446,7 @@ if (isset($_GET['edit_holiday_id'])) {
                                 echo '<td>' . htmlspecialchars($laboratory['event_color']) . '</td>';
                                 echo '<td>' . implode(", ", $managerNames) . '</td>';
                                 echo '<td>'; 
-                                echo '<a href="addons_model.php?name=event_callendar&id=' . htmlspecialchars($_GET['id']) . '&addon_event_edit_id=' . htmlspecialchars($laboratory['id']) . '"><i class="fa fa-edit"></i></a> ';
+                                echo '<a href="addons_model.php?name=event_callendar&id=' . htmlspecialchars($_GET['id']) . '&event_edit_id=' . htmlspecialchars($laboratory['id']) . '"  value="'.isset($eventToEdit).'"><i class="fa fa-edit"></i></a>';
                                 echo '<a href="../../cover/addons/event_callendar/action/delete_event.php?name=event_callendar&id=' . htmlspecialchars($_GET['id']) . '&event_id=' . htmlspecialchars($laboratory['id']) . '"><i class="fa fa-trash"></i></a>';
                                 echo '</td>';
                                 echo '</tr>';
@@ -472,14 +486,17 @@ if (isset($_GET['edit_holiday_id'])) {
 </div>
 
 <script>
+    
     $(document).ready(function() {
         $('#managerSelect').select2();
     });
     function confirmDelete(url) {
-        if (confirm("Ar tikrai norite trinti?")) {
+        if (confirm("Are you sure you want to delete??")) {
             window.location.href = url;
         }
     }
+
+
 
 function deleteEventAddon(addonId) {
     $("#deleteModal").modal("show");
@@ -502,5 +519,19 @@ function deleteEventAddon(addonId) {
         $("#deleteModal").modal("hide");
     });
 }
+document.addEventListener("DOMContentLoaded", function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const eventEditId = urlParams.get('event_edit_id');
+
+    if(eventEditId) {
+        var tab = new bootstrap.Tab(document.querySelector('a[href="#coordinators"]'));
+        tab.show();
+
+        var myAccordion = document.getElementById('accordionFlushExample');
+        var bsCollapse = new bootstrap.Collapse(myAccordion.querySelector('.collapse'), {
+            toggle: true
+        });
+    }
+});
 
 </script>
