f8cb97c45bf380e6a35224c3741bfe3105cebfe1
Darius Jakaitis <105962203+flusity@users.noreply.github.com>
2023-11-26 18:51:19 +0200

    Update index.php
    Signed-off-by: Darius Jakaitis <105962203+flusity@users.noreply.github.com>

--- index.php
+++ index.php
@@ -10,6 +10,9 @@ if (session_status() !== PHP_SESSION_ACTIVE) {
 
 require_once 'core/functions/functions.php';
 
+    header('X-Content-Type-Options: nosniff');
+    header('X-XSS-Protection: 1; mode=block');
+
 list($db, $config, $prefix) = initializeSystem();
 secureSession($db, $prefix);
 $settings = getSettings($db, $prefix);
