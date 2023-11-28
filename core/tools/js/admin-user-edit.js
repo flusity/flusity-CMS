c63bf7c877c8aa085abf1a050befe4eaff5b9c39
Darius Jakaitis <105962203+flusity@users.noreply.github.com>
2023-11-28 11:41:22 +0200

    Add files via upload
    Signed-off-by: Darius Jakaitis <105962203+flusity@users.noreply.github.com>

--- core/tools/js/admin-user-edit.js
+++ core/tools/js/admin-user-edit.js
@@ -13,7 +13,49 @@ function loadUserEditForm(userId) {
       });
 }
 
+
 function initializeEditUserForm() {
+    const editUserForm = document.querySelector('#edit-user-form');
+    if (editUserForm) {
+        editUserForm.addEventListener('submit', function (event) {
+            event.preventDefault();
+  
+            // Sukurkite FormData objektą iš formos
+            const formData = new FormData(event.target);
+  
+            // Įsitikinkite, kad CSRF žetonas yra įtrauktas
+            const csrfToken = editUserForm.querySelector('input[name="csrf_token"]').value;
+            if (csrfToken) {
+                formData.append('csrf_token', csrfToken);
+            } else {
+                console.error('CSRF token is missing');
+                return;
+            }
+  
+            fetch('update_user.php', {
+                method: 'POST',
+                body: formData
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (data.success) {
+                    window.location.href = 'users.php';
+                } else {
+                    alert('Error updating user.');
+                }
+            })
+            .catch(error => {
+                console.error('Klaida:', error);
+                alert('Error updating user.');
+            });
+        });
+    }
+  
+  
+
+
+
+/* function initializeEditUserForm() {
   const editUserForm = document.querySelector('#edit-user-form');
   if (editUserForm) {
       editUserForm.addEventListener('submit', function (event) {
@@ -38,7 +80,7 @@ function initializeEditUserForm() {
               alert('Error updating user.');
           });
       });
-  }
+  } */
 
   const cancelEditUserBtn = document.querySelector('#cancel-edit-user');
   if (cancelEditUserBtn) {
